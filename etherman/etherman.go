8b8ab9b526736c4493ed7bb31cb9eab87bd82630
Alonso <ARR551@protonmail.com>
2021-11-15 08:38:22 +0100

    etherman

--- etherman/etherman.go
+++ etherman/etherman.go
@@ -5,6 +5,7 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/hermeznetwork/hermez-core/etherman/smartcontracts/proofofefficiency"
+	"github.com/hermeznetwork/hermez-core/state"
 )
 type EtherMan struct {
     Poe *proofofefficiency.Proofofefficiency
@@ -13,33 +14,41 @@ type EtherMan struct {
 func NewEtherman() (*EtherMan, error) {
 	var address common.Address
 	var backend bind.ContractBackend
-	poe := proofofefficiency.NewProofofefficiency(address, backend)
+	poe, err := proofofefficiency.NewProofofefficiency(address, backend)
+	if err != nil {
+		return nil, err
+	}
 
 	return &EtherMan{Poe: poe}, nil
 }
 
 //This function retrieves the ethereum block information by ethereum block number
 func (etherMan *EtherMan) EthBlockByNumber(blockNum int64) (types.Block, error) {
-
+	//TODO
+	return types.Block{}, nil
 }
 
 //This function retrieves the batches information that are included in a specific ethereum block
-func (etherMan *EtherMan) GetBatchesByBlock(blockNum int64) ([]batch, error) {
-
+func (etherMan *EtherMan) GetBatchesByBlock(blockNum int64) ([]state.Batch, error) {
+	//TODO
+	return []state.Batch{}, nil
 }
 
 //This function retrieves the batches information that are included in all this ethereum blocks
 //from block x to block y
-func (etherMan *EtherMan) GetBatchesFromBlockTo(fromBlock uint, toBlock uint) ([]batch, error) {
-
+func (etherMan *EtherMan) GetBatchesFromBlockTo(fromBlock uint, toBlock uint) ([]state.Batch, error) {
+	//TODO
+	return []state.Batch{}, nil
 }
 
 //This function allows the sequencer send a new batch proposal to the rollup
-func (etherMan *EtherMan) sendBatch(batch) (txId, error) {
-
+func (etherMan *EtherMan) sendBatch(batch state.Batch) (common.Hash, error) {
+	//TODO
+	return common.Hash{}, nil
 }
 
 //This function allows the agregator send the proof for a batch and consolidate it
-func (etherMan *EtherMan) consolidateBatch(batch, proof) (txId, error) {
-
+func (etherMan *EtherMan) consolidateBatch(batch state.Batch, proof state.Proof) (common.Hash, error) {
+	//TODO
+	return common.Hash{}, nil
 }
