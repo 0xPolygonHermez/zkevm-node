9c4498f57b9a229d6b4c2f84ec29ecfcfe940844
Mikhail Wall <mikhwall@gmail.com>
2021-11-19 18:23:13 +0300

    added high level implementation for sequencer and aggregator (#6)
    * added high level implementation for sequencer and aggregator
    
    * added ethman support
    
    * fixed pr comments
    
    * merged with main
    
    * merged with main

--- sequencer/synchronizer.go
+++ sequencer/synchronizer.go
@@ -1,16 +1,20 @@
 package sequencer
 
+import (
+	"github.com/ethereum/go-ethereum/common"
+)
+
+type SyncEvent struct {
+	StartingHash common.Hash
+	BatchNum     uint64
+}
+
 type SynchronizerClient struct {
-	IsSynced chan int
+	SyncEventChan chan SyncEvent
 }
 
 func NewSynchronizerClient() SynchronizerClient {
 	// connect to synch to get signal, that it's synced
-	isSynced := make(chan int)
-	return SynchronizerClient{IsSynced: isSynced}
-}
-
-func (sc *SynchronizerClient) subscrToSyncedSignal() {
-	// get signal
-	sc.IsSynced <- 1
+	syncEventChan := make(chan SyncEvent)
+	return SynchronizerClient{SyncEventChan: syncEventChan}
 }
