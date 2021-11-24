39b7195deba3bea593304bfccc5c899975887ece
Thiago Coimbra Lemos <tclemos@users.noreply.github.com>
2021-11-24 13:53:15 -0300

    Add config to access private key from etherman (#48)
    Add private key decryption to sign L1 txs

--- aggregator/config.go
+++ aggregator/config.go
@@ -5,6 +5,5 @@ import "github.com/hermeznetwork/hermez-core/etherman"
 // Config represents the configuration of the aggregator
 type Config struct {
 	// PrivateKey is used to sign l1 tx
-	PrivateKey string
-	Etherman   etherman.Config
+	Etherman etherman.Config
 }
--- sequencer/config.go
+++ sequencer/config.go
@@ -4,7 +4,6 @@ import "github.com/hermeznetwork/hermez-core/etherman"
 
 // Config represents the configuration of a sequencer
 type Config struct {
-	PrivateKey                  string
 	SendBatchFrequencyInSeconds uint
 	Etherman                    etherman.Config
 }
