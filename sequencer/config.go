790955cb812e01114068466022e6f96992a04fd6
Mikhail Wall <mikhwall@gmail.com>
2021-12-01 13:55:20 +0300

    added sync with the state (#89)

--- sequencer/config.go
+++ sequencer/config.go
@@ -12,6 +12,10 @@ type Config struct {
 	// trying to propose a batch
 	IntervalToProposeBatch time.Duration
 
+	// SyncedBlockDif is the difference, how many block left to sync. So if sequencer see, that
+	// X amount of blocks are left to sync, it will start to select txs
+	SyncedBlockDif uint64
+
 	// Etherman is the configuration required by etherman to interact with L1
 	Etherman etherman.Config
 }
