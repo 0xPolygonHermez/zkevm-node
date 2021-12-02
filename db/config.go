2f1bbe0ea5d890fe23f44032d4b74ea8cf7be169
Oleksandr Brezhniev <oleksandr.brezhniev@gmail.com>
2021-12-02 02:48:17 +0200

    Possibility to read db config from well-known env vars

--- db/config.go
+++ db/config.go
@@ -1,5 +1,7 @@
 package db
 
+import "os"
+
 // Config provide fields to configure the pool
 type Config struct {
 	// Database name
@@ -16,4 +18,24 @@ type Config struct {
 
 	// Port Number
 	Port string
+}
+
+// NewConfigFromEnv creates config from standard postgres environment variables,
+// see https://www.postgresql.org/docs/11/libpq-envars.html for details
+func NewConfigFromEnv() Config {
+	return Config{
+		Database: getEnv("PGDATABASE", "polygon-hermez"),
+		User:     getEnv("PGUSER", "hermez"),
+		Password: getEnv("PGPASSWORD", "polygon"),
+		Host:     getEnv("PGHOST", "localhost"),
+		Port:     getEnv("PGPORT", "5432"),
+	}
+}
+
+func getEnv(key string, defaultValue string) string {
+	value, exists := os.LookupEnv(key)
+	if exists {
+		return value
+	}
+	return defaultValue
 }
