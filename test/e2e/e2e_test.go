84253105345b7f654b86f685f23236c69de1458b
Thiago Lemos <tclemos@outlook.com>
2021-12-23 10:09:32 -0300

    remove unused variable

--- test/e2e/e2e_test.go
+++ test/e2e/e2e_test.go
@@ -44,8 +44,6 @@ const (
 
 	l1AccHexAddress    = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
 	l1AccHexPrivateKey = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
-
-	keyStorePassword = "testonly"
 )
 
 var dbConfig = dbutils.NewConfigFromEnv()
