11e0929ee8771188b83f506f7fc1a9183dbe907b
Alonso Rodriguez <ARR552@users.noreply.github.com>
2021-12-28 11:21:18 +0100

    wait sync time delay when it's fully synced (#173)
    * wait sync time delay when it's fully synced
    
    * linter
    
    * WaitDuration as config param
    
    * linter
    
    * linter github actions update
    
    * #177 store last proposed batch into db
    
    * e2e fix

---
+++ synchronizer/config.go
@@ -0,0 +1,24 @@
+package synchronizer
+
+import "time"
+
+// Duration is a wrapper type that parses time duration from text.
+type Duration struct {
+	time.Duration `validate:"required"`
+}
+
+// UnmarshalText unmarshalls time duration from text.
+func (d *Duration) UnmarshalText(data []byte) error {
+	duration, err := time.ParseDuration(string(data))
+	if err != nil {
+		return err
+	}
+	d.Duration = duration
+	return nil
+}
+
+// Config represents the configuration of the synchronizer
+type Config struct {
+	// SyncInterval is the delay interval between reading new rollup information
+	SyncInterval Duration `mapstructure:"SyncInterval"`
+}
