e74c6a7d1c613ec6d4bfc85f7e13aa75e7aa0a89
Thiago Lemos <tclemos@outlook.com>
2022-01-26 14:25:47 -0300

    add debug_traceTransaction response

--- jsonrpc/debug.go
+++ jsonrpc/debug.go
@@ -1,7 +1,11 @@
 package jsonrpc
 
 import (
+	"context"
+	"errors"
+
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/hermeznetwork/hermez-core/state"
 )
 
@@ -10,8 +14,35 @@ type Debug struct {
 	state state.State
 }
 
+type traceTransactionResponse struct {
+	Gas         uint64        `json:"gas"`
+	Failed      bool          `json:"failed"`
+	ReturnValue string        `json:"returnValue"`
+	StructLogs  []interface{} `json:"structLogs"`
+}
+
 // TraceTransaction creates a response for debug_traceTransaction request.
 // See https://geth.ethereum.org/docs/rpc/ns-debug#debug_tracetransaction
 func (d *Debug) TraceTransaction(hash common.Hash) (interface{}, error) {
-	return nil, nil
+
+	ctx := context.Background()
+
+	tx, err := d.state.GetTransactionByHash(ctx, hash)
+	if errors.Is(err, state.ErrNotFound) {
+		return genesisIsNotTraceableError{}, nil
+	}
+
+	rcpt, err := d.state.GetTransactionReceipt(ctx, hash)
+	if errors.Is(err, state.ErrNotFound) {
+		return genesisIsNotTraceableError{}, nil
+	}
+
+	resp := traceTransactionResponse{
+		Gas:         tx.Gas(),
+		Failed:      rcpt.Status == types.ReceiptStatusFailed,
+		ReturnValue: "",
+		StructLogs:  []interface{}{},
+	}
+
+	return resp, nil
 }
