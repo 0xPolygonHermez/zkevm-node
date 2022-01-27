a23340a885ca7e503d478a84756aa597250477a1
ToniRamirezM <toni@iden3.com>
2022-01-27 18:22:43 +0100

    Store smart contract test

--- state/state_test.go
+++ state/state_test.go
@@ -18,7 +18,6 @@ import (
 	"github.com/hermeznetwork/hermez-core/log"
 	"github.com/hermeznetwork/hermez-core/state"
 	"github.com/hermeznetwork/hermez-core/state/pgstatestorage"
-	"github.com/hermeznetwork/hermez-core/state/runtime/evm"
 	"github.com/hermeznetwork/hermez-core/state/tree"
 	"github.com/hermeznetwork/hermez-core/test/dbutils"
 	"github.com/hermeznetwork/hermez-core/test/vectors"
@@ -956,18 +955,14 @@ func TestSCExecution(t *testing.T) {
 		SmartContracts: make(map[common.Address][]byte),
 	}
 
-	genesis.SmartContracts[addr] = []byte{
-		evm.PUSH1, 0x01, evm.PUSH1, 0x02, evm.ADD,
-		evm.PUSH1, 0x00, evm.MSTORE8,
-		evm.PUSH1, 0x01, evm.PUSH1, 0x00, evm.RETURN,
-	}
+	genesis.SmartContracts[addr] = common.Hex2Bytes("608060405234801561001057600080fd5b50610150806100206000396000f3fe608060405234801561001057600080fd5b50600436106100365760003560e01c80632e64cec11461003b5780636057361d14610059575b600080fd5b610043610075565b60405161005091906100d9565b60405180910390f35b610073600480360381019061006e919061009d565b61007e565b005b60008054905090565b8060008190555050565b60008135905061009781610103565b92915050565b6000602082840312156100b3576100b26100fe565b5b60006100c184828501610088565b91505092915050565b6100d3816100f4565b82525050565b60006020820190506100ee60008301846100ca565b92915050565b6000819050919050565b600080fd5b61010c816100f4565b811461011757600080fd5b5056fea2646970667358221220404e37f487a89a932dca5e77faaf6ca2de3b991f93d230604b1b8daaef64766264736f6c63430008070033")
 
 	err = st.SetGenesis(ctx, genesis)
 	require.NoError(t, err)
 
 	var txs []*types.Transaction
 
-	tx := types.NewTransaction(0, addr, big.NewInt(1000000000000000000), 10000, new(big.Int).SetUint64(1), nil)
+	tx := types.NewTransaction(0, addr, big.NewInt(0000000000000000000), 10000, new(big.Int).SetUint64(1), common.Hex2Bytes("6057361d0000000000000000000000000000000000000000000000000000000000000001"))
 
 	privateKey, err := crypto.HexToECDSA(strings.TrimPrefix(sequencerPvtKey, "0x"))
 	require.NoError(t, err)
@@ -1002,5 +997,5 @@ func TestSCExecution(t *testing.T) {
 	receipt, err := testState.GetTransactionReceipt(ctx, signedTx.Hash())
 	require.NoError(t, err)
 	// Compare against test receipt
-	assert.Equal(t, uint64(24), receipt.GasUsed)
+	assert.Equal(t, uint64(117), receipt.GasUsed)
 }
