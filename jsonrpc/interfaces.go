86e896931e1bb20c6bf4804f11770f63222ad3ed
Federico Gimenez <fgimenez@users.noreply.github.com>
2022-02-09 15:07:38 +0100

    Update jsonrpc/interfaces.go
    Co-authored-by: a_bennassar <arnaubennassar5@gmail.com>

--- jsonrpc/interfaces.go
+++ jsonrpc/interfaces.go
@@ -10,7 +10,7 @@ import (
 // Consumer interfaces required by the package.
 
 // jsonrpcTxPool contains the methods required to interact with the tx pool.
-type jsonrpcTxPool interface {
+type jsonRPCTxPool interface {
 	AddTx(ctx context.Context, tx types.Transaction) error
 	GetPendingTxs(ctx context.Context) ([]pool.Transaction, error)
 	GetGasPrice(ctx context.Context) (uint64, error)
