631ac3582be89d8cc5f58e16de52022a5bfc351e
Federico Gimenez <federico.gimenez@gmail.com>
2022-02-09 09:11:17 +0100

    added interfaces.go files

---
+++ jsonrpc/interfaces.go
@@ -0,0 +1,17 @@
+package jsonrpc
+
+import (
+	"context"
+
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/hermeznetwork/hermez-core/pool"
+)
+
+// Consumer interfaces required by the package.
+
+// jsonrpcTxPool contains the methods required to interact with the tx pool.
+type jsonrpcTxPool interface {
+	AddTx(ctx context.Context, tx types.Transaction) error
+	GetPendingTxs(ctx context.Context) ([]pool.Transaction, error)
+	GetGasPrice(ctx context.Context) (uint64, error)
+}
