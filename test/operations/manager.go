daa2c4a26540c67c71ae9ff3e7616eb36e65b479
Federico Gimenez <federico.gimenez@gmail.com>
2022-02-09 16:47:37 +0100

    Add MT service

--- test/operations/manager.go
+++ test/operations/manager.go
@@ -34,6 +34,7 @@ import (
 	"github.com/hermeznetwork/hermez-core/test/vectors"
 	"github.com/iden3/go-iden3-crypto/poseidon"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/health/grpc_health_v1"
 )
 
 const (
@@ -199,6 +200,14 @@ func (m *Manager) ApplyTxs(txs []vectors.Tx, initialRoot, finalRoot string) erro
 		return err
 	}
 	return nil
+}
+
+// WaitGRPCHealthy waits for a gRPC endpoint to be responding according to the
+// health standard in package grpc.health.v1
+func WaitGRPCHealthy(address string) error {
+	return waitPoll(defaultInterval, defaultDeadline, func() (bool, error) {
+		return grpcHealthyCondition(address)
+	})
 }
 
 // Setup creates all the required components and initializes them according to
@@ -599,6 +608,33 @@ func proverUpCondition() (bool, error) {
 
 func coreUpCondition() (done bool, err error) {
 	return nodeUpCondition(l2NetworkURL)
+}
+
+func grpcHealthyCondition(address string) (bool, error) {
+	opts := []grpc.DialOption{
+		grpc.WithInsecure(),
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
+	conn, err := grpc.DialContext(ctx, address, opts...)
+	if err != nil {
+		// we allow connection errors to wait for the container up
+		return false, nil
+	}
+	defer func() {
+		err = conn.Close()
+	}()
+
+	healthClient := grpc_health_v1.NewHealthClient(conn)
+	state, err := healthClient.Check(context.Background(), &grpc_health_v1.HealthCheckRequest{})
+	if err != nil {
+		// we allow connection errors to wait for the container up
+		return false, nil
+	}
+
+	done := state.Status == grpc_health_v1.HealthCheckResponse_SERVING
+
+	return done, nil
 }
 
 func waitPoll(interval, deadline time.Duration, condition conditionFunc) error {
