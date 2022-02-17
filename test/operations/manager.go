89ccad3f4ca5758bafc68012a2872dee28144c2a
Alonso <ARR551@protonmail.com>
2022-02-17 17:39:40 +0100

    addresses

--- test/operations/manager.go
+++ test/operations/manager.go
@@ -41,10 +41,10 @@ const (
 	l1NetworkURL = "http://localhost:8545"
 	l2NetworkURL = "http://localhost:8123"
 
-	poeAddress            = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9"
-	bridgeAddress         = "0xffffffffffffffffffffffffffffffffffffffff"
+	poeAddress            = "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9"
+	bridgeAddress         = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9"
 	maticTokenAddress     = "0x5FbDB2315678afecb367f032d93F642f64180aa3" //nolint:gosec
-	globalExitRootAddress = "0xffffffffffffffffffffffffffffffffffffffff"
+	globalExitRootAddress = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0"
 
 	l1AccHexAddress    = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
 	l1AccHexPrivateKey = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
