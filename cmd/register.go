1fb9791fb38ff545b9f64fc5e58eeda52bc415ea
Federico Gimenez <federico.gimenez@gmail.com>
2022-02-24 13:30:05 +0100

    use current mt implementation in register

--- cmd/register.go
+++ cmd/register.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"context"
 	"errors"
 	"fmt"
 	"strings"
@@ -12,10 +11,8 @@ import (
 	"github.com/hermeznetwork/hermez-core/state"
 	"github.com/hermeznetwork/hermez-core/state/pgstatestorage"
 	"github.com/hermeznetwork/hermez-core/state/tree"
-	"github.com/hermeznetwork/hermez-core/state/tree/pb"
 	"github.com/iden3/go-iden3-crypto/poseidon"
 	"github.com/urfave/cli/v2"
-	"google.golang.org/grpc"
 )
 
 func registerSequencer(ctx *cli.Context) error {
@@ -63,19 +60,8 @@ func registerSequencer(ctx *cli.Context) error {
 		MaxCumulativeGasUsed: c.NetworkConfig.MaxCumulativeGasUsed,
 	}
 
-	srvCfg := &tree.ServerConfig{
-		Host: c.MTServer.Host,
-		Port: c.MTServer.Port,
-	}
-	s := grpc.NewServer()
-	mtSrv := tree.NewServer(srvCfg, tr)
-	pb.RegisterMTServiceServer(s, mtSrv)
-
-	mtClient, _, _ := newMTClient(c.MTClient)
-	treeAdapter := tree.NewAdapter(context.Background(), mtClient)
-
 	stateDb := pgstatestorage.NewPostgresStorage(sqlDB)
-	st := state.NewState(stateCfg, stateDb, treeAdapter)
+	st := state.NewState(stateCfg, stateDb, tr)
 
 	_, err = st.GetSequencer(ctx.Context, etherman.GetAddress())
 	if errors.Is(err, state.ErrNotFound) { //If It doesn't exist, register the sequencer
