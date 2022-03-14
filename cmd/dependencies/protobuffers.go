e301defd622dc2a0824586e6605b9d66173f5c45
Federico Gimenez <fgimenez@users.noreply.github.com>
2022-03-14 10:57:12 +0100

    Automated update of proto files and stub generation (#472)
    * Automated update of proto files and stub generation
    
    * rename pb -> proto

---
+++ cmd/dependencies/protobuffers.go
@@ -0,0 +1,64 @@
+package dependencies
+
+import (
+	"os"
+	"os/exec"
+
+	"github.com/hermeznetwork/hermez-core/log"
+	"github.com/spf13/afero"
+)
+
+const (
+	defaultPBSourceRepo    = "git@github.com:hermeznetwork/comms-protocol.git"
+	defaultPBTargetDirPath = "../../proto/src"
+)
+
+type pbUpdater struct {
+	fs afero.Fs
+
+	gm *githubManager
+
+	sourceRepo    string
+	targetDirPath string
+}
+
+func init() {
+	aferoFs := afero.NewOsFs()
+
+	gm := newGithubManager(aferoFs, os.Getenv("UPDATE_DEPS_SSH_PK"), os.Getenv("GITHUB_TOKEN"))
+
+	pb := &pbUpdater{
+		fs: aferoFs,
+
+		gm: gm,
+
+		sourceRepo:    defaultPBSourceRepo,
+		targetDirPath: defaultPBTargetDirPath,
+	}
+
+	dependenciesList = append(dependenciesList, pb)
+}
+
+func (pb *pbUpdater) update() error {
+	log.Infof("Cloning %q...", pb.sourceRepo)
+	tmpdir, err := pb.gm.cloneTargetRepo(pb.sourceRepo)
+	if err != nil {
+		return err
+	}
+
+	targetDirPath := getTargetPath(pb.targetDirPath)
+
+	log.Infof("Updating files %q...", pb.sourceRepo)
+	err = updateFiles(pb.fs, tmpdir, targetDirPath)
+	if err != nil {
+		return err
+	}
+
+	log.Infof("Generating stubs from proto files...")
+
+	c := exec.Command("make", "generate-code-from-proto")
+	c.Dir = "."
+	c.Stdout = os.Stdout
+	c.Stderr = os.Stderr
+	return c.Run()
+}
