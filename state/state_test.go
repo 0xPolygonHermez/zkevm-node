6444b74876926aeb6236cf4c4a47aa0683f3b3b8
Federico Gimenez <federico.gimenez@gmail.com>
2022-03-28 15:58:53 +0200

    Query initial state root and update hardcoded expected final root

--- state/state_test.go
+++ state/state_test.go
@@ -957,7 +957,6 @@ func TestSCExecution(t *testing.T) {
 	var sequencerPvtKey = "0x28b2b0318721be8c8339199172cd7cc8f5e273800a35616ec893083a4b32c02e"
 	var sequencerBalance = 400000
 	var scAddress = common.HexToAddress("0x1275fbb540c8efC58b812ba83B0D0B8b9917AE98")
-	var stateRoot = "0x23f74ec0030d8307f32eb1fd2e088d2efb9f7dff8d28e45fbdd4e55f6137eeab"
 
 	// Init database instance
 	err := dbutils.InitOrReset(cfg)
@@ -1050,8 +1049,9 @@ func TestSCExecution(t *testing.T) {
 	}
 
 	// Create Batch Processor
-	bp, err := st.NewBatchProcessor(ctx, sequencerAddress, common.Hex2Bytes(strings.TrimPrefix(stateRoot, "0x")))
-
+	stateRoot, err := testState.GetStateRoot(ctx, true)
+	require.NoError(t, err)
+	bp, err := st.NewBatchProcessor(ctx, sequencerAddress, stateRoot)
 	require.NoError(t, err)
 
 	err = bp.ProcessBatch(ctx, batch)
@@ -1084,8 +1084,7 @@ func TestSCCall(t *testing.T) {
 	scInteractionByteCode, err := compilesc.ReadBytecode("interaction.sol")
 	require.NoError(t, err)
 	var scInteractionAddress = common.HexToAddress("0x85e844b762A271022b692CF99cE5c59BA0650Ac8")
-	var stateRoot = "0x236a5c853ae354e96f6d52b8b40bf46d4348b1ea10364a9de93b68c7b5e40444"
-	var expectedFinalRoot = "5241619567610880215670173716655616355071093812240119030254379390542265998597"
+	var expectedFinalRoot = "19932175360494665424565192343786508796232379317287730915918310098824732983695"
 
 	// Init database instance
 	err = dbutils.InitOrReset(cfg)
@@ -1209,8 +1208,9 @@ func TestSCCall(t *testing.T) {
 	}
 
 	// Create Batch Processor
-
-	bp, err := st.NewBatchProcessor(ctx, sequencerAddress, common.Hex2Bytes(strings.TrimPrefix(stateRoot, "0x")))
+	stateRoot, err := testState.GetStateRoot(ctx, true)
+	require.NoError(t, err)
+	bp, err := st.NewBatchProcessor(ctx, sequencerAddress, stateRoot)
 	require.NoError(t, err)
 
 	err = bp.ProcessBatch(ctx, batch)
@@ -1377,7 +1377,6 @@ func TestEmitLog(t *testing.T) {
 	scByteCode, err := compilesc.ReadBytecode("emitLog.sol")
 	require.NoError(t, err)
 	var scAddress = common.HexToAddress("0x1275fbb540c8efC58b812ba83B0D0B8b9917AE98")
-	var stateRoot = "0x28bca78c7bed7bb292fb40355053c2ac3f9f1c566d32b3f25acae23d64762f24"
 
 	// Init database instance
 	err = dbutils.InitOrReset(cfg)
@@ -1466,7 +1465,9 @@ func TestEmitLog(t *testing.T) {
 	}
 
 	// Create Batch Processor
-	bp, err := st.NewBatchProcessor(ctx, sequencerAddress, common.Hex2Bytes(strings.TrimPrefix(stateRoot, "0x")))
+	stateRoot, err := testState.GetStateRoot(ctx, true)
+	require.NoError(t, err)
+	bp, err := st.NewBatchProcessor(ctx, sequencerAddress, stateRoot)
 	require.NoError(t, err)
 
 	err = bp.ProcessBatch(ctx, batch)
@@ -1726,7 +1727,6 @@ func TestEstimateGas(t *testing.T) {
 	var sequencerPvtKey = "0x28b2b0318721be8c8339199172cd7cc8f5e273800a35616ec893083a4b32c02e"
 	var sequencerBalance = 400000
 	var scAddress = common.HexToAddress("0x1275fbb540c8efC58b812ba83B0D0B8b9917AE98")
-	var stateRoot = "0x23f74ec0030d8307f32eb1fd2e088d2efb9f7dff8d28e45fbdd4e55f6137eeab"
 
 	// Init database instance
 	err := dbutils.InitOrReset(cfg)
@@ -1810,7 +1810,9 @@ func TestEstimateGas(t *testing.T) {
 	}
 
 	// Create Batch Processor
-	bp, err := st.NewBatchProcessor(ctx, sequencerAddress, common.Hex2Bytes(strings.TrimPrefix(stateRoot, "0x")))
+	stateRoot, err := testState.GetStateRoot(ctx, true)
+	require.NoError(t, err)
+	bp, err := st.NewBatchProcessor(ctx, sequencerAddress, stateRoot)
 	require.NoError(t, err)
 
 	err = bp.ProcessBatch(ctx, batch)
