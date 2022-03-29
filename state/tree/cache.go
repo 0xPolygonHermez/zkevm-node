0463d192242b6bd51cf06888fded5bcce122c0a6
Federico Gimenez <federico.gimenez@gmail.com>
2022-03-29 15:13:14 +0200

    Add MT node cache

--- state/tree/cache.go
+++ state/tree/cache.go
@@ -1,6 +1,13 @@
 package tree
 
-import "github.com/dgraph-io/ristretto"
+import (
+	"errors"
+	"fmt"
+	"sync"
+
+	"github.com/dgraph-io/ristretto"
+	poseidon "github.com/iden3/go-iden3-crypto/goldenposeidon"
+)
 
 const (
 	cacheNumCounters = 1e7     // number of keys to track frequency of (10M).
@@ -20,4 +27,90 @@ func NewStoreCache() (*ristretto.Cache, error) {
 		return nil, err
 	}
 	return cache, err
+}
+
+// nodeCache represents a cache layer to store nodes during a db transaction in
+// the merkletree.
+type nodeCache struct {
+	lock   *sync.Mutex
+	data   map[string][]uint64
+	active bool
+}
+
+const (
+	maxMTNodeCacheEntries = 256
+)
+
+var (
+	errMTNodeCacheItemNotFound = errors.New("MT node cache item not found")
+)
+
+// newNodeCache is the nodeCache constructor.
+func newNodeCache() *nodeCache {
+	return &nodeCache{
+		lock: &sync.Mutex{},
+		data: make(map[string][]uint64),
+	}
+}
+
+// get reads a MT node cache entry.
+func (nc *nodeCache) get(key []uint64) ([]uint64, error) {
+	if len(key) != poseidon.CAPLEN {
+		return nil, fmt.Errorf("Invalid key length should be %d", poseidon.CAPLEN)
+	}
+	keyStr := h4ToString(key)
+
+	item, ok := nc.data[keyStr]
+	if !ok {
+		return nil, errMTNodeCacheItemNotFound
+	}
+	return item, nil
+}
+
+// set inserts a new MT node cache entry.
+func (nc *nodeCache) set(key []uint64, value []uint64) error {
+	if len(nc.data) >= maxMTNodeCacheEntries {
+		return errors.New("MT node cache is full")
+	}
+	if len(key) != poseidon.CAPLEN {
+		return fmt.Errorf("Invalid key length, should be %d", poseidon.CAPLEN)
+	}
+	keyStr := h4ToString(key)
+
+	nc.lock.Lock()
+	defer nc.lock.Unlock()
+
+	nc.data[keyStr] = value
+
+	return nil
+}
+
+// clear removes all the entries of the MT node cache.
+func (nc *nodeCache) clear() {
+	nc.lock.Lock()
+	defer nc.lock.Unlock()
+
+	nc.data = make(map[string][]uint64)
+}
+
+// isActive is the active field getter.
+func (nc *nodeCache) isActive() bool {
+	return nc.active
+}
+
+// setActive is the active field setter.
+func (nc *nodeCache) setActive(active bool) {
+	nc.active = active
+}
+
+// init initializes the MT node cache.
+func (nc *nodeCache) init() {
+	nc.clear()
+	nc.setActive(true)
+}
+
+// teardown resets the MT node cache.
+func (nc *nodeCache) teardown() {
+	nc.clear()
+	nc.setActive(false)
 }
