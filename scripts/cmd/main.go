f3110b8f2029bbb1d24bc19afec228e12dca16d8
Federico Gimenez <federico.gimenez@gmail.com>
2022-04-11 10:42:25 +0200

    Improvements in SC compilation

--- scripts/cmd/main.go
+++ scripts/cmd/main.go
@@ -7,6 +7,10 @@ import (
 	"github.com/urfave/cli/v2"
 )
 
+const (
+	flagInput = "input"
+)
+
 func main() {
 	app := cli.NewApp()
 	app.Name = "hermez-node-scripts"
@@ -16,6 +20,19 @@ func main() {
 			Usage:  "Updates external dependencies like images, test vectors or proto files",
 			Action: updateDeps,
 			Flags:  []cli.Flag{},
+		},
+		{
+			Name:   "compilesc",
+			Usage:  "Compiles smart contracts required for testing",
+			Action: compileSC,
+			Flags: []cli.Flag{
+				&cli.StringFlag{
+					Name:     flagInput,
+					Aliases:  []string{"in"},
+					Usage:    "Target path where the source solidity files are located. It can be a file or a directory, in which case the command will traverse all the descendants from it.",
+					Required: true,
+				},
+			},
 		},
 	}
 
