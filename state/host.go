c1bdca2a30fd3d84dd5ac0bc48ce596ec3380372
Toni Ramírez <adramirez@gmail.com>
2022-04-26 18:51:08 +0200

    fix

--- state/host.go
+++ state/host.go
@@ -215,7 +215,12 @@ func (h *Host) Callx(ctx context.Context, contract *runtime.Contract, host runti
 		}
 	}
 
-	contract2 := runtime.NewContractCall(contract.Depth+1, contract.Caller, contract.CodeAddress, contract.Address, contract.Value, contract.Gas, contract.Code, contract.Input)
+	contract2 := runtime.NewContractCall(contract.Depth+1, contract.Address, contract.Caller, contract.CodeAddress, contract.Value, contract.Gas, contract.Code, contract.Input)
+
+	if contract.Type == runtime.DelegateCall {
+		contract2 = runtime.NewContractCall(contract.Depth+1, contract.Caller, contract.CodeAddress, contract.Address, contract.Value, contract.Gas, contract.Code, contract.Input)
+	}
+
 	result := h.run(ctx, contract2)
 
 	return result
