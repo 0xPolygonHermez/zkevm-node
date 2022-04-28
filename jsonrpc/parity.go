0cc028e0837d53ecd4a946dfe84eb6c01c7738ec
Thiago Lemos <tclemos@outlook.com>
2022-04-28 07:02:33 -0300

    add comments to parity struct

--- jsonrpc/parity.go
+++ jsonrpc/parity.go
@@ -1,7 +1,10 @@
 package jsonrpc
 
+// Parity contains implementations for the "parity" RPC endpoints
 type Parity struct{}
 
+// PendingTransactions creates a response for parity_pendingTransactions request.
+// See https://openethereum.github.io/JSONRPC-parity-module#parity_pendingtransactions
 func (p *Parity) PendingTransactions() (interface{}, error) {
 	return []interface{}{}, nil
 }
