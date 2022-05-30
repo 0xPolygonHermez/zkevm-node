43ba030151421f0bbfbe9df7fd055b358a564bfe
Federico Gimenez <fgimenez@users.noreply.github.com>
2022-05-30 10:37:43 +0200

    Fixed bridge deposit address (#682)
    * Fixed bridge deposit address
    
    * only fund sequencer if funding values are set
    
    * make bridge deposit constants public

--- scripts/init_network/main.go
+++ scripts/init_network/main.go
@@ -8,40 +8,23 @@ import (
 	NW "github.com/hermeznetwork/hermez-core/tools/network"
 )
 
-const (
-	l1NetworkURL = "http://localhost:8545"
-	l2NetworkURL = "http://localhost:8123"
-
-	l1BridgeAddr = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9"
-	l2BridgeAddr = "0x9d98deabc42dd696deb9e40b4f1cab7ddbf55988"
-
-	l1AccHexAddress    = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
-	l1AccHexPrivateKey = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
-
-	sequencerAddress    = "0x617b3a3528F9cDd6630fd3301B9c8911F7Bf063D"
-	sequencerPrivateKey = "0x28b2b0318721be8c8339199172cd7cc8f5e273800a35616ec893083a4b32c02e"
-
-	bridgeDepositReceiverAddress    = "0xc949254d682d8c9ad5682521675b8f43b102aec4"
-	bridgeDepositReceiverPrivateKey = "0xdfd01798f92667dbf91df722434e8fbe96af0211d4d1b82bbbbc8f1def7a814f"
-
-	txTimeout = 60 * time.Second
-)
-
 func main() {
 	ctx := context.Background()
 	if err := NW.InitNetwork(ctx,
 		NW.InitNetworkConfig{
-			L1NetworkURL:                    l1NetworkURL,
-			L2NetworkURL:                    l2NetworkURL,
-			L1BridgeAddr:                    l1BridgeAddr,
-			L2BridgeAddr:                    l2BridgeAddr,
-			L1AccHexAddress:                 l1AccHexAddress,
-			L1AccHexPrivateKey:              l1AccHexPrivateKey,
-			SequencerAddress:                sequencerAddress,
-			SequencerPrivateKey:             sequencerPrivateKey,
-			BridgeDepositReceiverAddress:    bridgeDepositReceiverAddress,
-			BridgeDepositReceiverPrivateKey: bridgeDepositReceiverPrivateKey,
-			TxTimeout:                       txTimeout,
+			L1NetworkURL: "http://localhost:8545",
+			L2NetworkURL: "http://localhost:8123",
+			L1BridgeAddr: "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9",
+			L2BridgeAddr: "0x9d98deabc42dd696deb9e40b4f1cab7ddbf55988",
+			L1Deployer: NW.L1Deployer{
+				Address:                  "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
+				PrivateKey:               "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
+				L1ETHAmountToSequencer:   "200000000000000000000",
+				L1MaticAmountToSequencer: "200000000000000000000000",
+			},
+			SequencerAddress:    "0x617b3a3528F9cDd6630fd3301B9c8911F7Bf063D",
+			SequencerPrivateKey: "0x28b2b0318721be8c8339199172cd7cc8f5e273800a35616ec893083a4b32c02e",
+			TxTimeout:           time.Minute,
 		}); err != nil {
 		log.Fatal(err)
 	}
