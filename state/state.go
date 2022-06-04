e425891f6e82bcb6547069fb7a84659e81f48ec5
Toni Ramírez <adramirez@gmail.com>
2022-06-04 21:20:09 +0200

    Fix revert reason message

--- state/state.go
+++ state/state.go
@@ -286,12 +286,6 @@ func (s *State) EstimateGas(transaction *types.Transaction, senderAddress common
 		}
 	}
 
-	// Checks if executor level valid gas errors occurred
-	/*
-		isGasApplyError := func(err error) bool {
-			return errors.As(err, &ErrNotEnoughIntrinsicGas)
-		}*/
-
 	// Checks if EVM level valid gas errors occurred
 	isGasEVMError := func(err error) bool {
 		return errors.Is(err, runtime.ErrOutOfGas) ||
