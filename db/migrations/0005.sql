4fad983a1cd1295c32af19f86054123ee89bbcd9
Cool Developer <cool199966@outlook.com>
2022-06-13 13:47:10 +0100

    add the transaction part

--- db/migrations/0005.sql
+++ db/migrations/0005.sql
@@ -1,52 +1,63 @@
 -- +migrate Up
-CREATE SCHEMA statev2;
-
-CREATE TABLE statev2.block
-(
-    block_num   BIGINT PRIMARY KEY,
-    block_hash  BYTEA                       NOT NULL,
-    parent_hash BYTEA,
-
+CREATE SCHEMA statev2 CREATE TABLE statev2.block (
+    block_num BIGINT PRIMARY KEY,
+    block_hash VARCHAR NOT NULL,
+    parent_hash VARCHAR,
     received_at TIMESTAMP WITH TIME ZONE NOT NULL
 );
 
-CREATE TABLE statev2.batch (
+CREATE TABLE statev2.batch (  --batch abstraction 
     id SERIAL PRIMARY KEY,
-    global_exit_root BYTEA,
+    global_exit_root VARCHAR,
+    batch_hash VARCHAR,
+    tx_hash VARCHAR,
     block_num BIGINT NOT NULL REFERENCES statev2.block (block_num) ON DELETE CASCADE
 );
 
-CREATE TABLE statev2.sequenced_batch (
+CREATE TABLE statev2.sequenced_batch (  --virtual state
     batch_num BIGINT PRIMARY KEY,
-    force_batches_num BIGINT,
-    sequencer BYTEA,
+    force_batches_num INTEGER,  --if empty, it means this batch is from sequenceForceBatches
+    tx_hash VARCHAR,
+    sequencer VARCHAR,
     sequenced_at TIMESTAMP WITH TIME ZONE NOT NULL,
     block_num BIGINT NOT NULL REFERENCES statev2.block (block_num) ON DELETE CASCADE,
-    batch_id BIGINT NOT NULL REFERENCES statev2.batch (id) ON DELETE CASCADE
+    batch_id BIGINT NOT NULL REFERENCES statev2.batch (id)
 );
 
-CREATE TABLE statev2.verified_batch (
+CREATE TABLE statev2.verified_batch (  --consolidated state
     batch_num BIGINT PRIMARY KEY,
-    aggregator BYTEA,
-    consolidated_at TIMESTAMP WITH TIME ZONE NOT NULL,
-    block_num BIGINT NOT NULL REFERENCES statev2.block (block_num) ON DELETE CASCADE,
-    batch_id BIGINT NOT NULL REFERENCES statev2.batch (id) ON DELETE CASCADE
+    tx_hash VARCHAR,
+    aggregator VARCHAR,
+    consolidated_at TIMESTAMP WITH TIME ZONE,
+    batch_id BIGINT NOT NULL REFERENCES statev2.batch (id),
+    block_num BIGINT NOT NULL REFERENCES statev2.block (block_num) ON DELETE CASCADE
 );
 
 CREATE TABLE statev2.forced_batch (
     forced_batch_num BIGINT PRIMARY KEY,
-    sequencer BYTEA,
+    sequencer VARCHAR,
     forced_at TIMESTAMP WITH TIME ZONE NOT NULL,
     batch_id BIGINT NOT NULL REFERENCES statev2.batch (id) ON DELETE CASCADE
 );
 
-CREATE TABLE statev2.transaction (
-    tx_hash BYTEA PRIMARY KEY,
-    tx_index BIGINT,
-    from_address BYTEA,
+-- All txs must be stored here. We only will manage the relationship between transactions and batches regarding the reorg.
+CREATE TABLE statev2.tx_batch_relationship (
+    tx_id BIGINT NOT NULL REFERENCES statev2.transaction (id) ON DELETE CASCADE,
+    batch_id BIGINT NOT NULL REFERENCES statev2.batch (id) ON DELETE CASCADE
+);
+
+CREATE TABLE statev2.transaction (  --transaction abstraction
+    id SERIAL PRIMARY KEY,
+    tx_hash VARCHAR,
+    from_address VARCHAR,
     encoded VARCHAR,
     decoded jsonb,
+    received_at TIMESTAMP WITH TIME ZONE NOT NULL
+);
+
+CREATE TABLE statev2.ts_transaction (  --transaction in the trusted sequencer
+    tx_id BIGINT NOT NULL REFERENCES statev2.transaction (id) ON DELETE CASCADE,
+    tx_index BIGINT PRIMARY KEY,  --l2Block identifier
     header jsonb,
-    uncles jsonb,
-    batch_id BIGINT NOT NULL REFERENCES statev2.batch (id) ON DELETE CASCADE
+    uncles jsonb
 );
