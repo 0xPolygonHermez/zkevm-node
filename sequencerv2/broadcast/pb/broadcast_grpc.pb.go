8633ac6fd538d2efb8f593efad1a77dfa672a4e8
Federico Gimenez <federico.gimenez@gmail.com>
2022-06-15 16:26:34 +0200

    updated generated code

--- sequencerv2/broadcast/pb/broadcast_grpc.pb.go
+++ sequencerv2/broadcast/pb/broadcast_grpc.pb.go
@@ -22,9 +22,8 @@ const _ = grpc.SupportPackageIsVersion7
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type BroadcastServiceClient interface {
-	GetLastBatchNumber(ctx context.Context, in *Empty, opts ...grpc.CallOption) (*GetLastBatchNumberResponse, error)
-	GetTransactionsPerBatch(ctx context.Context, in *GetTransactionsPerBatchRequest, opts ...grpc.CallOption) (*GetTransactionsPerBatchResponse, error)
-	GetLastBatchTransactions(ctx context.Context, in *Empty, opts ...grpc.CallOption) (*GetLastBatchTransactionsResponse, error)
+	GetLastBatch(ctx context.Context, in *Empty, opts ...grpc.CallOption) (*GetBatchResponse, error)
+	GetBatch(ctx context.Context, in *GetBatchRequest, opts ...grpc.CallOption) (*GetBatchResponse, error)
 }
 
 type broadcastServiceClient struct {
@@ -35,27 +34,18 @@ func NewBroadcastServiceClient(cc grpc.ClientConnInterface) BroadcastServiceClie
 	return &broadcastServiceClient{cc}
 }
 
-func (c *broadcastServiceClient) GetLastBatchNumber(ctx context.Context, in *Empty, opts ...grpc.CallOption) (*GetLastBatchNumberResponse, error) {
-	out := new(GetLastBatchNumberResponse)
-	err := c.cc.Invoke(ctx, "/broadcast.v1.BroadcastService/GetLastBatchNumber", in, out, opts...)
+func (c *broadcastServiceClient) GetLastBatch(ctx context.Context, in *Empty, opts ...grpc.CallOption) (*GetBatchResponse, error) {
+	out := new(GetBatchResponse)
+	err := c.cc.Invoke(ctx, "/broadcast.v1.BroadcastService/GetLastBatch", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-func (c *broadcastServiceClient) GetTransactionsPerBatch(ctx context.Context, in *GetTransactionsPerBatchRequest, opts ...grpc.CallOption) (*GetTransactionsPerBatchResponse, error) {
-	out := new(GetTransactionsPerBatchResponse)
-	err := c.cc.Invoke(ctx, "/broadcast.v1.BroadcastService/GetTransactionsPerBatch", in, out, opts...)
-	if err != nil {
-		return nil, err
-	}
-	return out, nil
-}
-
-func (c *broadcastServiceClient) GetLastBatchTransactions(ctx context.Context, in *Empty, opts ...grpc.CallOption) (*GetLastBatchTransactionsResponse, error) {
-	out := new(GetLastBatchTransactionsResponse)
-	err := c.cc.Invoke(ctx, "/broadcast.v1.BroadcastService/GetLastBatchTransactions", in, out, opts...)
+func (c *broadcastServiceClient) GetBatch(ctx context.Context, in *GetBatchRequest, opts ...grpc.CallOption) (*GetBatchResponse, error) {
+	out := new(GetBatchResponse)
+	err := c.cc.Invoke(ctx, "/broadcast.v1.BroadcastService/GetBatch", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -66,9 +56,8 @@ func (c *broadcastServiceClient) GetLastBatchTransactions(ctx context.Context, i
 // All implementations must embed UnimplementedBroadcastServiceServer
 // for forward compatibility
 type BroadcastServiceServer interface {
-	GetLastBatchNumber(context.Context, *Empty) (*GetLastBatchNumberResponse, error)
-	GetTransactionsPerBatch(context.Context, *GetTransactionsPerBatchRequest) (*GetTransactionsPerBatchResponse, error)
-	GetLastBatchTransactions(context.Context, *Empty) (*GetLastBatchTransactionsResponse, error)
+	GetLastBatch(context.Context, *Empty) (*GetBatchResponse, error)
+	GetBatch(context.Context, *GetBatchRequest) (*GetBatchResponse, error)
 	mustEmbedUnimplementedBroadcastServiceServer()
 }
 
@@ -76,14 +65,11 @@ type BroadcastServiceServer interface {
 type UnimplementedBroadcastServiceServer struct {
 }
 
-func (UnimplementedBroadcastServiceServer) GetLastBatchNumber(context.Context, *Empty) (*GetLastBatchNumberResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method GetLastBatchNumber not implemented")
+func (UnimplementedBroadcastServiceServer) GetLastBatch(context.Context, *Empty) (*GetBatchResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetLastBatch not implemented")
 }
-func (UnimplementedBroadcastServiceServer) GetTransactionsPerBatch(context.Context, *GetTransactionsPerBatchRequest) (*GetTransactionsPerBatchResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method GetTransactionsPerBatch not implemented")
-}
-func (UnimplementedBroadcastServiceServer) GetLastBatchTransactions(context.Context, *Empty) (*GetLastBatchTransactionsResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method GetLastBatchTransactions not implemented")
+func (UnimplementedBroadcastServiceServer) GetBatch(context.Context, *GetBatchRequest) (*GetBatchResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetBatch not implemented")
 }
 func (UnimplementedBroadcastServiceServer) mustEmbedUnimplementedBroadcastServiceServer() {}
 
@@ -98,56 +84,38 @@ func RegisterBroadcastServiceServer(s grpc.ServiceRegistrar, srv BroadcastServic
 	s.RegisterService(&BroadcastService_ServiceDesc, srv)
 }
 
-func _BroadcastService_GetLastBatchNumber_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _BroadcastService_GetLastBatch_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(Empty)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(BroadcastServiceServer).GetLastBatchNumber(ctx, in)
+		return srv.(BroadcastServiceServer).GetLastBatch(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/broadcast.v1.BroadcastService/GetLastBatchNumber",
+		FullMethod: "/broadcast.v1.BroadcastService/GetLastBatch",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(BroadcastServiceServer).GetLastBatchNumber(ctx, req.(*Empty))
+		return srv.(BroadcastServiceServer).GetLastBatch(ctx, req.(*Empty))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-func _BroadcastService_GetTransactionsPerBatch_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(GetTransactionsPerBatchRequest)
+func _BroadcastService_GetBatch_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(GetBatchRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(BroadcastServiceServer).GetTransactionsPerBatch(ctx, in)
+		return srv.(BroadcastServiceServer).GetBatch(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/broadcast.v1.BroadcastService/GetTransactionsPerBatch",
+		FullMethod: "/broadcast.v1.BroadcastService/GetBatch",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(BroadcastServiceServer).GetTransactionsPerBatch(ctx, req.(*GetTransactionsPerBatchRequest))
-	}
-	return interceptor(ctx, in, info, handler)
-}
-
-func _BroadcastService_GetLastBatchTransactions_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(Empty)
-	if err := dec(in); err != nil {
-		return nil, err
-	}
-	if interceptor == nil {
-		return srv.(BroadcastServiceServer).GetLastBatchTransactions(ctx, in)
-	}
-	info := &grpc.UnaryServerInfo{
-		Server:     srv,
-		FullMethod: "/broadcast.v1.BroadcastService/GetLastBatchTransactions",
-	}
-	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(BroadcastServiceServer).GetLastBatchTransactions(ctx, req.(*Empty))
+		return srv.(BroadcastServiceServer).GetBatch(ctx, req.(*GetBatchRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
@@ -160,16 +128,12 @@ var BroadcastService_ServiceDesc = grpc.ServiceDesc{
 	HandlerType: (*BroadcastServiceServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
-			MethodName: "GetLastBatchNumber",
-			Handler:    _BroadcastService_GetLastBatchNumber_Handler,
+			MethodName: "GetLastBatch",
+			Handler:    _BroadcastService_GetLastBatch_Handler,
 		},
 		{
-			MethodName: "GetTransactionsPerBatch",
-			Handler:    _BroadcastService_GetTransactionsPerBatch_Handler,
-		},
-		{
-			MethodName: "GetLastBatchTransactions",
-			Handler:    _BroadcastService_GetLastBatchTransactions_Handler,
+			MethodName: "GetBatch",
+			Handler:    _BroadcastService_GetBatch_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
