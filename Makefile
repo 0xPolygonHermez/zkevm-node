f88e76aaa68c722151fce33052e324f5c8a6fddc
Federico Gimenez <federico.gimenez@gmail.com>
2022-06-16 11:37:38 +0200

    Broadcast server and client implementation

--- Makefile
+++ Makefile
@@ -221,6 +221,7 @@ generate-mocks: ## Generates mocks for the tests, using mockery tool
 	mockery --name=stateInterface --dir=jsonrpc --output=jsonrpc --outpkg=jsonrpc --inpackage --structname=stateMock --filename=mock_state_test.go
 	mockery --name=BatchProcessorInterface --dir=jsonrpc --output=jsonrpc --outpkg=jsonrpc --inpackage --structname=batchProcessorMock --filename=mock_batchProcessor_test.go
 	mockery --name=txManager --dir=sequencerv2 --output=sequencerv2 --outpkg=sequencerv2 --structname=txmanagerMock --filename=txmanager-mock_test.go
+	mockery --name=stateInterface --dir=sequencerv2/broadcast --output=sequencerv2/broadcast --outpkg=broadcast --structname=stateMock --filename=state-mock_test.go
 
 .PHONY: generate-code-from-proto
 generate-code-from-proto: ## Generates code from proto files
---
+++ sequencerv2/broadcast/server_test.go
@@ -0,0 +1,211 @@
+package broadcast
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"os"
+	"path"
+	"runtime"
+	"testing"
+	"time"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/hermeznetwork/hermez-core/sequencerv2/broadcast/pb"
+	"github.com/hermeznetwork/hermez-core/test/operations"
+	"github.com/hermeznetwork/hermez-core/test/testutils"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+const (
+	host = "0.0.0.0"
+	port = 61091
+)
+
+var (
+	address      = fmt.Sprintf("%s:%d", host, port)
+	broadcastSrv *Server
+	conn         *grpc.ClientConn
+	cancel       context.CancelFunc
+	err          error
+	ctx          = context.Background()
+)
+
+func init() {
+	// Change dir to project root
+	// This is important because we have relative paths to files containing test vectors
+	_, filename, _, _ := runtime.Caller(0)
+	dir := path.Join(path.Dir(filename), "../../")
+	err := os.Chdir(dir)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TestMain(m *testing.M) {
+	initialize()
+	defer teardown()
+
+	os.Exit(m.Run())
+}
+
+func initialize() {
+	broadcastSrv = initBroadcastServer()
+	go broadcastSrv.Start()
+
+	conn, cancel, err = initConn()
+	if err != nil {
+		panic(err)
+	}
+
+	err = operations.WaitGRPCHealthy(address)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func teardown() {
+	cancel()
+	broadcastSrv.Stop()
+}
+
+func initConn() (*grpc.ClientConn, context.CancelFunc, error) {
+	opts := []grpc.DialOption{
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	}
+	ctx, cancel := context.WithTimeout(ctx, 1*time.Second)
+	conn, err := grpc.DialContext(ctx, address, opts...)
+	return conn, cancel, err
+}
+
+func initBroadcastServer() *Server {
+	s := grpc.NewServer()
+	st := new(stateMock)
+	cfg := &ServerConfig{
+		Host: host,
+		Port: port,
+	}
+
+	broadcastSrv = NewServer(cfg, st)
+	pb.RegisterBroadcastServiceServer(s, broadcastSrv)
+
+	return broadcastSrv
+}
+
+func TestBroadcastServerGetBatch(t *testing.T) {
+	tcs := []struct {
+		description        string
+		inputBatchNumber   uint64
+		expectedBatch      *Batch
+		expectedEncodedTxs []string
+		expectedErr        bool
+		expectedErrMsg     string
+	}{
+		{
+			description:      "happy path",
+			inputBatchNumber: 14,
+			expectedBatch: &Batch{
+				BatchNumber:    14,
+				GlobalExitRoot: common.Hash{},
+				Timestamp:      time.Now(),
+			},
+			expectedEncodedTxs: []string{"tx1", "tx2", "tx3"},
+		},
+		{
+			description:      "query errors are returned",
+			inputBatchNumber: 14,
+			expectedErr:      true,
+			expectedErrMsg:   "query error",
+		},
+	}
+
+	for _, tc := range tcs {
+		tc := tc
+		t.Run(tc.description, func(t *testing.T) {
+			st := new(stateMock)
+			var err error
+			if tc.expectedErr {
+				err = errors.New(tc.expectedErrMsg)
+			}
+			st.On("GetBatchByNumber", mock.AnythingOfType("*context.valueCtx"), tc.inputBatchNumber, nil).Return(tc.expectedBatch, err)
+			st.On("GetEncodedTransactionsByBatchNumber", mock.AnythingOfType("*context.valueCtx"), tc.inputBatchNumber, nil).Return(tc.expectedEncodedTxs, err)
+
+			broadcastSrv.SetState(st)
+
+			client := pb.NewBroadcastServiceClient(conn)
+			actualBatch, err := client.GetBatch(ctx, &pb.GetBatchRequest{
+				BatchNumber: tc.inputBatchNumber,
+			})
+			require.NoError(t, testutils.CheckError(err, tc.expectedErr, fmt.Sprintf("rpc error: code = Unknown desc = %s", tc.expectedErrMsg)))
+
+			if err == nil {
+				require.Equal(t, tc.expectedBatch.BatchNumber, actualBatch.BatchNumber)
+				require.Equal(t, tc.expectedBatch.GlobalExitRoot.String(), actualBatch.GlobalExitRoot)
+				require.Equal(t, uint64(tc.expectedBatch.Timestamp.Unix()), actualBatch.Timestamp)
+				for i, encoded := range tc.expectedEncodedTxs {
+					require.Equal(t, encoded, actualBatch.Transactions[i].Encoded)
+				}
+				require.True(t, st.AssertExpectations(t))
+			}
+		})
+	}
+}
+
+func TestBroadcastServerGetLastBatch(t *testing.T) {
+	tcs := []struct {
+		description        string
+		expectedBatch      *Batch
+		expectedEncodedTxs []string
+		expectedErr        bool
+		expectedErrMsg     string
+	}{
+		{
+			description: "happy path",
+			expectedBatch: &Batch{
+				BatchNumber:    14,
+				GlobalExitRoot: common.Hash{},
+				Timestamp:      time.Now(),
+			},
+			expectedEncodedTxs: []string{"tx1", "tx2", "tx3"},
+		},
+		{
+			description:    "query errors are returned",
+			expectedErr:    true,
+			expectedErrMsg: "query error",
+		},
+	}
+
+	for _, tc := range tcs {
+		tc := tc
+		t.Run(tc.description, func(t *testing.T) {
+			st := new(stateMock)
+			var err error
+			if tc.expectedErr {
+				err = errors.New(tc.expectedErrMsg)
+			}
+			st.On("GetLastBatch", mock.AnythingOfType("*context.valueCtx"), nil).Return(tc.expectedBatch, err)
+			if tc.expectedBatch != nil {
+				st.On("GetEncodedTransactionsByBatchNumber", mock.AnythingOfType("*context.valueCtx"), tc.expectedBatch.BatchNumber, nil).Return(tc.expectedEncodedTxs, err)
+			}
+
+			broadcastSrv.SetState(st)
+
+			client := pb.NewBroadcastServiceClient(conn)
+			actualBatch, err := client.GetLastBatch(ctx, &pb.Empty{})
+			require.NoError(t, testutils.CheckError(err, tc.expectedErr, fmt.Sprintf("rpc error: code = Unknown desc = %s", tc.expectedErrMsg)))
+
+			if err == nil {
+				require.Equal(t, tc.expectedBatch.BatchNumber, actualBatch.BatchNumber)
+				require.Equal(t, tc.expectedBatch.GlobalExitRoot.String(), actualBatch.GlobalExitRoot)
+				require.Equal(t, uint64(tc.expectedBatch.Timestamp.Unix()), actualBatch.Timestamp)
+				for i, encoded := range tc.expectedEncodedTxs {
+					require.Equal(t, encoded, actualBatch.Transactions[i].Encoded)
+				}
+				require.True(t, st.AssertExpectations(t))
+			}
+		})
+	}
+}
