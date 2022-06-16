f0571c25b65e5a750c0aa6c24b8bb74f5372e427
tclemos <thiago@iden3.com>
2022-06-16 16:28:22 -0300

    fix eth_syncing to use information about the virtual state instead of the consolidated one

--- state/syncinginfo.go
+++ state/syncinginfo.go
@@ -2,7 +2,8 @@ package state
 
 // SyncingInfo stores information regarding the syncing status of the node
 type SyncingInfo struct {
-	LastBatchNumberSeen         uint64 `json:"lastBatchNumberSeen"`
-	LastBatchNumberConsolidated uint64 `json:"lastBatchNumberConsolidated"`
-	InitialSyncingBatch         uint64 `json:"initialSyncingBatch"`
+	InitialSyncingBatch         uint64
+	LastBatchNumberSeen         uint64
+	LastBatchNumberConsolidated uint64
+	CurrentBatchNumber          uint64
 }
