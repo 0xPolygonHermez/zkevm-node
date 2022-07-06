3d66a10b19ab7f998bb911762735ad5b999bad72
Toni Ramírez <adramirez@gmail.com>
2022-07-06 12:43:14 +0200

    WIP

--- statev2/state_test.go
+++ statev2/state_test.go
@@ -2,19 +2,27 @@ package statev2_test
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"math/big"
 	"os"
+	"strings"
 	"testing"
 	"time"
 
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/hermeznetwork/hermez-core/db"
+	"github.com/hermeznetwork/hermez-core/log"
 	"github.com/hermeznetwork/hermez-core/merkletree"
 	state "github.com/hermeznetwork/hermez-core/statev2"
 	"github.com/hermeznetwork/hermez-core/statev2/runtime/executor"
 	executorclientpb "github.com/hermeznetwork/hermez-core/statev2/runtime/executor/pb"
 	"github.com/hermeznetwork/hermez-core/test/dbutils"
+	"github.com/hermeznetwork/hermez-core/test/testutils"
 	"github.com/jackc/pgx/v4/pgxpool"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -22,7 +30,7 @@ import (
 )
 
 const (
-// ether155V = 27
+	ether155V = 27
 )
 
 var (
@@ -247,17 +255,21 @@ func TestAddVirtualBatch(t *testing.T) {
 	require.NoError(t, tx.Commit(ctx))
 }
 
-/*
 func TestExecuteTransaction(t *testing.T) {
 	var chainIDSequencer = new(big.Int).SetInt64(400)
 	var sequencerAddress = common.HexToAddress("0x617b3a3528F9cDd6630fd3301B9c8911F7Bf063D")
 	var sequencerPvtKey = "0x28b2b0318721be8c8339199172cd7cc8f5e273800a35616ec893083a4b32c02e"
 	var sequencerBalance = 4000000
+	var scCounterAddress = common.HexToAddress("0x1275fbb540c8efC58b812ba83B0D0B8b9917AE98")
+	var scInteractionAddress = common.HexToAddress("0x85e844b762A271022b692CF99cE5c59BA0650Ac8")
+
 	scCounterByteCode, err := testutils.ReadBytecode("Counter/Counter.bin")
+	require.NoError(t, err)
+	scInteractionByteCode, err := testutils.ReadBytecode("Interaction/Interaction.bin")
 	require.NoError(t, err)
 
 	// Deploy counter.sol
-	tx := types.NewTx(&types.LegacyTx{
+	tx0 := types.NewTx(&types.LegacyTx{
 		Nonce:    0,
 		To:       nil,
 		Value:    new(big.Int),
@@ -271,29 +283,48 @@ func TestExecuteTransaction(t *testing.T) {
 	auth, err := bind.NewKeyedTransactorWithChainID(privateKey, chainIDSequencer)
 	require.NoError(t, err)
 
-	signedTx, err := auth.Signer(auth.From, tx)
+	signedTx0, err := auth.Signer(auth.From, tx0)
 	require.NoError(t, err)
 
-	// Encode transaction
-	v, r, s := signedTx.RawSignatureValues()
-	sign := 1 - (v.Uint64() & 1)
-
-	txCodedRlp, err := rlp.EncodeToBytes([]interface{}{
-		signedTx.Nonce(),
-		signedTx.GasPrice(),
-		signedTx.Gas(),
-		signedTx.To(),
-		signedTx.Value(),
-		signedTx.Data(),
-		signedTx.ChainId(), uint(0), uint(0),
+	// Deploy interaction.sol
+	tx1 := types.NewTx(&types.LegacyTx{
+		Nonce:    0,
+		To:       nil,
+		Value:    new(big.Int),
+		Gas:      uint64(sequencerBalance),
+		GasPrice: new(big.Int).SetUint64(0),
+		Data:     common.Hex2Bytes(scInteractionByteCode),
 	})
+
+	signedTx1, err := auth.Signer(auth.From, tx1)
 	require.NoError(t, err)
 
-	newV := new(big.Int).Add(big.NewInt(ether155V), big.NewInt(int64(sign)))
-	newRPadded := fmt.Sprintf("%064s", r.Text(hex.Base))
-	newSPadded := fmt.Sprintf("%064s", s.Text(hex.Base))
-	newVPadded := fmt.Sprintf("%02s", newV.Text(hex.Base))
-	batchL2Data, err := hex.DecodeString(hex.EncodeToString(txCodedRlp) + newRPadded + newSPadded + newVPadded)
+	// Call setCounterAddr method from Interaction SC to set Counter SC Address
+	tx2 := types.NewTransaction(2, scInteractionAddress, new(big.Int), 40000, new(big.Int).SetUint64(1), common.Hex2Bytes("ec39b429000000000000000000000000"+strings.TrimPrefix(scCounterAddress.String(), "0x")))
+	signedTx2, err := auth.Signer(auth.From, tx2)
+	require.NoError(t, err)
+
+	// Increment Counter calling Counter SC
+	tx3 := types.NewTransaction(3, scCounterAddress, new(big.Int), 40000, new(big.Int).SetUint64(1), common.Hex2Bytes("d09de08a"))
+	signedTx3, err := auth.Signer(auth.From, tx3)
+	require.NoError(t, err)
+
+	// Retrieve counter value calling Interaction SC (this is the real test as Interaction SC will call Counter SC)
+	tx4 := types.NewTransaction(4, scInteractionAddress, new(big.Int), 40000, new(big.Int).SetUint64(1), common.Hex2Bytes("a87d942c"))
+	signedTx4, err := auth.Signer(auth.From, tx4)
+	require.NoError(t, err)
+
+	// Increment Counter calling again
+	tx5 := types.NewTransaction(5, scCounterAddress, new(big.Int), 40000, new(big.Int).SetUint64(1), common.Hex2Bytes("d09de08a"))
+	signedTx5, err := auth.Signer(auth.From, tx5)
+	require.NoError(t, err)
+
+	// Retrieve counter value again
+	tx6 := types.NewTransaction(6, scInteractionAddress, new(big.Int), 40000, new(big.Int).SetUint64(1), common.Hex2Bytes("a87d942c"))
+	signedTx6, err := auth.Signer(auth.From, tx6)
+	require.NoError(t, err)
+
+	batchL2Data, err := state.EncodeTransactions([]types.Transaction{*signedTx0, *signedTx1, *signedTx2, *signedTx3, *signedTx4, *signedTx5, *signedTx6})
 	require.NoError(t, err)
 
 	// Create Batch
@@ -310,43 +341,38 @@ func TestExecuteTransaction(t *testing.T) {
 		GenerateCallTrace:    0,
 	}
 
-	log.Debugf("%v", processBatchRequest)
-
 	processBatchResponse, err := executorClient.ProcessBatch(ctx, processBatchRequest)
 	require.NoError(t, err)
 
 	file, _ := json.MarshalIndent(processBatchResponse, "", " ")
 	err = ioutil.WriteFile("trace.json", file, 0644)
 	require.NoError(t, err)
+
 }
-*/
-/*
-func TestGenesis(t *testing.T) {
-	balances := map[common.Address]*big.Int{
-		common.HexToAddress("0xb1D0Dc8E2Ce3a93EB2b32f4C7c3fD9dDAf1211FA"): big.NewInt(1000),
-		common.HexToAddress("0xb1D0Dc8E2Ce3a93EB2b32f4C7c3fD9dDAf1211FB"): big.NewInt(2000),
+
+func TestExecutor(t *testing.T) {
+
+	// Create Batch
+	processBatchRequest := &executorclientpb.ProcessBatchRequest{
+		BatchNum:             1,
+		Coinbase:             common.HexToAddress("0x617b3a3528F9cDd6630fd3301B9c8911F7Bf063D").String(),
+		BatchL2Data:          common.Hex2Bytes("ee80843b9aca00830186a0944d5cf5032b2a844602278b01199ed191a86c93ff88016345785d8a0000808203e880801cee7e01dc62f69a12c3510c6d64de04ee6346d84b6a017f3e786c7d87f963e75d8cc91fa983cd6d9cf55fff80d73bd26cd333b0f098acc1e58edb1fd484ad731b"),
+		OldStateRoot:         common.Hex2Bytes("2dc4db4293af236cb329700be43f08ace740a05088f8c7654736871709687e90"),
+		GlobalExitRoot:       common.Hex2Bytes("090bcaf734c4f06c93954a827b45a6e8c67b8e0fd1e0a35a1c5982d6961828f9"),
+		OldLocalExitRoot:     common.Hex2Bytes("17c04c3760510b48c6012742c540a81aba4bca2f78b9d14bfd2f123e2e53ea3e"),
+		EthTimestamp:         uint64(1944498031),
+		UpdateMerkleTree:     0,
+		GenerateExecuteTrace: 0,
+		GenerateCallTrace:    0,
 	}
 
-	nonces := map[common.Address]*big.Int{
-		common.HexToAddress("0xb1D0Dc8E2Ce3a93EB2b32f4C7c3fD9dDAf1211FA"): big.NewInt(1),
-		common.HexToAddress("0xb1D0Dc8E2Ce3a93EB2b32f4C7c3fD9dDAf1211FB"): big.NewInt(1),
-	}
+	processBatchResponse, err := executorClient.ProcessBatch(ctx, processBatchRequest)
+	require.NoError(t, err)
 
-	smartContracts := map[common.Address][]byte{
-		common.HexToAddress("0xae4bb80be56b819606589de61d5ec3b522eeb032"): common.Hex2Bytes("608060405234801561001057600080fd5b50600436106100675760003560e01c806333d6247d1161005057806333d6247d146100a85780633ed691ef146100bd578063a3c573eb146100d257600080fd5b806301fd90441461006c5780633381fe9014610088575b600080fd5b61007560015481565b6040519081526020015b60405180910390f35b6100756100963660046101c7565b60006020819052908152604090205481565b6100bb6100b63660046101c7565b610117565b005b43600090815260208190526040902054610075565b6002546100f29073ffffffffffffffffffffffffffffffffffffffff1681565b60405173ffffffffffffffffffffffffffffffffffffffff909116815260200161007f565b60025473ffffffffffffffffffffffffffffffffffffffff1633146101c2576040517f08c379a000000000000000000000000000000000000000000000000000000000815260206004820152603460248201527f476c6f62616c45786974526f6f744d616e616765724c323a3a7570646174654560448201527f786974526f6f743a204f4e4c595f425249444745000000000000000000000000606482015260840160405180910390fd5b600155565b6000602082840312156101d957600080fd5b503591905056fea2646970667358221220d6ed73b81f538d38669b0b750b93be08ca365978fae900eedc9ca93131c97ca664736f6c63430008090033"),
-	}
+	log.Debugf("request:%v", processBatchRequest)
+	log.Debugf("new_state_root=%v", common.BigToHash(new(big.Int).SetBytes(processBatchResponse.NewStateRoot)))
 
-	storage := map[common.Address]map[*big.Int]*big.Int{
-		common.HexToAddress("0xae4bb80be56b819606589de61d5ec3b522eeb032"): {new(big.Int).SetBytes(common.Hex2Bytes("0000000000000000000000000000000000000000000000000000000000000002")): new(big.Int).SetBytes(common.Hex2Bytes("9d98deabc42dd696deb9e40b4f1cab7ddbf55988"))},
-	}
-
-	genesis := state.Genesis{
-		Balances:       balances,
-		Nonces:         nonces,
-		SmartContracts: smartContracts,
-		Storage:        storage,
-	}
-	err := testState.SetGenesis(ctx, genesis, nil)
+	file, _ := json.MarshalIndent(processBatchResponse, "", " ")
+	err = ioutil.WriteFile("trace.json", file, 0644)
 	require.NoError(t, err)
 }
-*/
