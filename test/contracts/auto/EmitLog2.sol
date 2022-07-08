b90b43175878e83ff2e1e3b679632eef8f0434d6
Toni Ramírez <adramirez@gmail.com>
2022-07-08 12:12:50 +0200

    update contract

--- test/contracts/auto/EmitLog2.sol
+++ test/contracts/auto/EmitLog2.sol
@@ -2,12 +2,8 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
-contract EmitLog {
-    event Log();
+contract EmitLog2 {
     event LogA(uint256 indexed a);
-    event LogAB(uint256 indexed a, uint256 indexed b);
-    event LogABC(uint256 indexed a, uint256 indexed b, uint256 indexed c);
-    event LogABCD(uint256 indexed a, uint256 indexed b, uint256 indexed c, uint256 d);
 
     function emitLogs() public {
         emit LogA(1);
