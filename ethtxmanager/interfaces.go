f2e963f80ae2a1bee9c982c568cf8d2f9dc023ae
Mikhail Wall <mikhwall@gmail.com>
2022-07-14 14:15:03 +0400

    updated aggregator to cmd-v2 (#890)
    * updated aggregator to cmd-v2
    
    * adding comments to state methods

--- ethtxmanager/interfaces.go
+++ ethtxmanager/interfaces.go
@@ -4,12 +4,15 @@ import (
 	"context"
 
 	ethmanTypes "github.com/0xPolygonHermez/zkevm-node/etherman/types"
+	"github.com/0xPolygonHermez/zkevm-node/proverclient/pb"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 )
 
 type etherman interface {
 	SequenceBatches(sequences []ethmanTypes.Sequence, gasLimit uint64) (*types.Transaction, error)
+	VerifyBatch(batchNumber uint64, resGetProof *pb.GetProofResponse, gasLimit uint64) (*types.Transaction, error)
+	EstimateGasForVerifyBatch(batchNumber uint64, resGetProof *pb.GetProofResponse) (uint64, error)
 	EstimateGasSequenceBatches(sequences []ethmanTypes.Sequence) (uint64, error)
 	GetTx(ctx context.Context, txHash common.Hash) (*types.Transaction, bool, error)
 	GetTxReceipt(ctx context.Context, txHash common.Hash) (*types.Receipt, error)
