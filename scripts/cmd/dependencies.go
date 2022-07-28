972a9cc581278f4c64e7204690c99be0ea9a5b9c
Federico Gimenez <federico.gimenez@gmail.com>
2022-07-28 12:47:35 +0200

    update repo URIs

--- scripts/cmd/dependencies.go
+++ scripts/cmd/dependencies.go
@@ -13,11 +13,11 @@ func updateDeps(ctx *cli.Context) error {
 		},
 		PB: &dependencies.PBConfig{
 			TargetDirPath: "../../../proto/src",
-			SourceRepo:    "git@github.com:0xPolygonHermez/zkevm-comms-protocol.git",
+			SourceRepo:    "https://github.com/0xPolygonHermez/zkevm-comms-protocol.git",
 		},
 		TV: &dependencies.TVConfig{
 			TargetDirPath: "../../../test/vectors/src",
-			SourceRepo:    "git@github.com:0xPolygonHermez/zkevm-testvectors.git",
+			SourceRepo:    "https://github.com/0xPolygonHermez/zkevm-testvectors.git",
 		},
 	}
 
