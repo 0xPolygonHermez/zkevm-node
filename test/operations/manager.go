92faeae168d669a8680185c68726fbb42b140b29
Alonso <ARR551@protonmail.com>
2022-10-14 09:56:42 +0200

    group 3

--- test/operations/manager.go
+++ test/operations/manager.go
@@ -397,7 +397,7 @@ func (m *Manager) SetUpSequencer() error {
 		return err
 	}
 
-	if b.Cmp(maticAmount) <= 0 {
+	if b.Cmp(maticAmount) < 0 {
 		return fmt.Errorf("Minimum amount is: %v but found: %v", maticAmount.Text(encoding.Base10), b.Text(encoding.Base10))
 	}
 
