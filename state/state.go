50a147e894128b5f59465f8ee245caf4c2cee3bc
Toni Ramírez <58293609+ToniRamirezM@users.noreply.github.com>
2022-10-20 11:36:59 +0200

    Properly check tx hash in CheckSupersetBatchTransactions (#1127)
    * Properly check tx hash in CheckSupersetBatchTransactions
    
    * correct docker-compose file

--- state/state.go
+++ state/state.go
@@ -1275,7 +1275,7 @@ func CheckSupersetBatchTransactions(existingTxHashes []common.Hash, processedTxs
 		return ErrExistingTxGreaterThanProcessedTx
 	}
 	for i, existingTxHash := range existingTxHashes {
-		if existingTxHash != processedTxs[i].Tx.Hash() {
+		if existingTxHash != processedTxs[i].TxHash {
 			return ErrOutOfOrderProcessedTx
 		}
 	}
