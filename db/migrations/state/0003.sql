bca88951b3598e642a5d0edf3f41c0cf3982528c
tclemos <thiago@iden3.com>
2022-10-21 19:31:19 -0300

    fix sql

--- db/migrations/state/0003.sql
+++ db/migrations/state/0003.sql
@@ -3,7 +3,7 @@ DROP TABLE state.sequences_group;
 DROP TABLE state.sequences;
 
 -- +migrate Up
-CREATE TABLE state.pending_sequences
+CREATE TABLE state.sequences
 (
     batch_num        BIGINT NOT NULL PRIMARY KEY REFERENCES state.batch (batch_num) ON DELETE CASCADE,
     state_root       VARCHAR NOT NULL,
@@ -13,7 +13,7 @@ CREATE TABLE state.pending_sequences
     txs              VARCHAR[] NOT NULL,
 );
 
-CREATE TABLE state.pending_sequences_groups
+CREATE TABLE state.sequences_groups
 (
     tx_hash     VARCHAR,
     tx_encoded  jsonb,
@@ -24,13 +24,13 @@ CREATE TABLE state.pending_sequences_groups
     PRIMARY KEY (tx_hash)
 );
 
-CREATE FUNCTION state.del_sequences_group_cascade_batch() RETURNS trigger AS $del_sequences_group_cascade_batch$
+CREATE FUNCTION state.fn_del_sequences_groups_cascade_batch() RETURNS trigger AS $fn_del_sequences_groups_cascade_batch$
     BEGIN
-        DELETE state.sequences_group
+        DELETE state.sequences_groups
          WHERE OLD.batch_num = ANY(batch_nums);
         RETURN OLD;
     END;
-$del_sequences_group_cascade_batch$ LANGUAGE plpgsql;
+$fn_del_sequences_groups_cascade_batch$ LANGUAGE plpgsql;
 
-CREATE TRIGGER state.del_seq_group BEFORE DELETE ON state.batch
-   FOR EACH ROW EXECUTE FUNCTION state.del_sequences_group_cascade_batch();
+CREATE TRIGGER state.tr_del_sequences_groups_cascade_batch BEFORE DELETE ON state.batch
+   FOR EACH ROW EXECUTE FUNCTION state.fn_del_sequences_groups_cascade_batch();
