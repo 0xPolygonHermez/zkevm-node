85221c0d861788aef453b8e161ea722bdd590590
Paolo Grisoli <paolo_g84@icloud.com>
2022-10-26 00:41:28 +0200

    test: refac ethtxman tests to meet new signatures
    SequenceBatches and VerifyBatch have new signatures and thus they can be
    better tested by checking the error instead of filtering the logs.

--- ethtxmanager/ethtxmanager_test.go
+++ ethtxmanager/ethtxmanager_test.go
@@ -7,10 +7,7 @@ import (
 
 	ethman "github.com/0xPolygonHermez/zkevm-node/etherman"
 	ethmanTypes "github.com/0xPolygonHermez/zkevm-node/etherman/types"
-	"github.com/0xPolygonHermez/zkevm-node/log"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
-	"go.uber.org/zap/zapcore"
 )
 
 func TestIncreaseGasLimit(t *testing.T) {
@@ -24,39 +21,19 @@ func TestIncreaseGasPrice(t *testing.T) {
 }
 
 func TestSequenceBatchesWithROEthman(t *testing.T) {
-	observedLogs, err := log.InitTestLogger(log.Config{Level: "INFO"})
-	require.NoError(t, err)
-	defer log.DeinitTestLogger()
-	// add a dummy log to increase the number of logs and verify that the
-	// filter works
-	log.Infof("bla")
 	ethManRO, _, _, _, _ := ethman.NewSimulatedEtherman(ethman.Config{}, nil)
 	txMan := New(Config{MaxSendBatchTxRetries: 2}, ethManRO) // 3 executions in total
 
-	txMan.SequenceBatches(context.Background(), []ethmanTypes.Sequence{})
+	err := txMan.SequenceBatches(context.Background(), []ethmanTypes.Sequence{})
 
-	observedLogs = observedLogs.
-		FilterLevelExact(zapcore.ErrorLevel).
-		FilterMessageSnippet(ethman.ErrIsReadOnlyMode.Error())
-	logs := observedLogs.All()
-	assert.Len(t, logs, 3)
+	assert.ErrorIs(t, err, ethman.ErrIsReadOnlyMode)
 }
 
 func TestVerifyBatchWithROEthman(t *testing.T) {
-	observedLogs, err := log.InitTestLogger(log.Config{Level: "INFO"})
-	require.NoError(t, err)
-	defer log.DeinitTestLogger()
-	// add a dummy log to increase the number of logs and verify that the
-	// filter works
-	log.Infof("bla")
 	ethManRO, _, _, _, _ := ethman.NewSimulatedEtherman(ethman.Config{}, nil)
 	txMan := New(Config{MaxVerifyBatchTxRetries: 2}, ethManRO) // 3 executions in total
 
-	txMan.VerifyBatch(context.Background(), 42, nil)
+	err := txMan.VerifyBatch(context.Background(), 42, nil)
 
-	observedLogs = observedLogs.
-		FilterLevelExact(zapcore.ErrorLevel).
-		FilterMessageSnippet(ethman.ErrIsReadOnlyMode.Error())
-	logs := observedLogs.All()
-	assert.Len(t, logs, 3)
+	assert.ErrorIs(t, err, ethman.ErrIsReadOnlyMode)
 }
