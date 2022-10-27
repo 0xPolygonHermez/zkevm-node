58fa58b687d9c57f8f425e3370b7f3b0920a564a
KonradIT <github@chernowii.com>
2022-10-27 13:54:24 +0200

    Naming change

--- state/runtime/executor/client.go
+++ state/runtime/executor/client.go
@@ -23,23 +23,23 @@ func NewExecutorClient(ctx context.Context, c Config) (pb.ExecutorServiceClient,
 	const maxRetries = 5
 	ctx, cancel := context.WithTimeout(ctx, maxWaitSeconds*time.Second)
 
-	connections := 0
+	connectionRetries := 0
 
 	var executorConn *grpc.ClientConn
 	var err error
-	for connections < maxRetries {
+	for connectionRetries < maxRetries {
 		log.Infof("trying to connect to executor: %v", c.URI)
 		executorConn, err = grpc.DialContext(ctx, c.URI, opts...)
 		if err != nil {
-			log.Infof("Retrying connection to executor #%d", connections)
-			connections = connections + 1
+			log.Infof("Retrying connection to executor #%d", connectionRetries)
+			connectionRetries = connectionRetries + 1
 		} else {
 			log.Infof("connected to executor")
 			break
 		}
 	}
 
-	if connections == maxRetries {
+	if connectionRetries == maxRetries {
 		log.Fatalf("fail to dial: %v", err)
 	}
 	executorClient := pb.NewExecutorServiceClient(executorConn)
