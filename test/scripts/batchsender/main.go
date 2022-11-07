bd8be099597521e4ea5f209e220b960cfb4c1ec6
Paolo Grisoli <paolo_g84@icloud.com>
2022-11-07 16:59:00 +0100

    fix: batch tx properly sent and recognized
    This commit fixes the L1 transaction so that it gets correctly
    recognized by the synchronizer and thus verified by the aggregator.
    Credits to @ARR552 for the fix.
    
    Signed-off-by: Paolo Grisoli <paolo_g84@icloud.com>

--- test/scripts/batchsender/main.go
+++ test/scripts/batchsender/main.go
@@ -3,14 +3,16 @@ package main
 import (
 	"bytes"
 	"context"
-	"math/big"
+	"fmt"
+	"os"
+	"strconv"
+	"time"
 
 	"github.com/0xPolygonHermez/zkevm-node/config"
 	"github.com/0xPolygonHermez/zkevm-node/etherman"
 	"github.com/0xPolygonHermez/zkevm-node/etherman/types"
 	"github.com/0xPolygonHermez/zkevm-node/log"
 	"github.com/0xPolygonHermez/zkevm-node/test/operations"
-	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/common"
 	ethtypes "github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethclient"
@@ -19,6 +21,25 @@ import (
 )
 
 func main() {
+	// send 1 batch by default or read the number of batches from args
+	nBatches := 1
+	usage := `Usage: batchsender <nbatches>
+ nbatches	Number of batches to be sent (defaults to 1)`
+	if len(os.Args) > 1 {
+		var err error
+		arg := os.Args[1]
+		if arg == "--help" || arg == "-h" {
+			fmt.Println(usage)
+			os.Exit(0)
+		}
+		nBatches, err = strconv.Atoi(arg)
+		if err != nil {
+			fmt.Println(usage)
+			os.Exit(1)
+		}
+	}
+
+	// retrieve default configuration
 	var cfg config.Config
 	viper.SetConfigType("toml")
 	err := viper.ReadConfig(bytes.NewBuffer([]byte(config.DefaultValues)))
@@ -37,28 +58,35 @@ func main() {
 
 	ctx := context.Background()
 
-	amount := big.NewInt(10000)
-	toAddress := common.HexToAddress("0x70997970C51812dc3A010C7d01b50e0d17dc79C8")
-	// toAddress := cfg.Etherman.PoEAddr
+	for i := 0; i < nBatches; i++ {
+		currentBlock, err := client.BlockByNumber(ctx, nil)
+		checkErr(err)
+		log.Debug("currentBlock.Time(): ", currentBlock.Time())
 
-	log.Debug("estimating gas limit")
-	gasLimit, err := client.EstimateGas(ctx, ethereum.CallMsg{From: auth.From, To: &toAddress, Value: amount})
-	checkErr(err)
+		seqAddr, err := ethMan.GetPublicAddress()
+		checkErr(err)
+		log.Info("Using address: ", seqAddr)
 
-	log.Debug("estimating gas price")
-	gasPrice, err := client.SuggestGasPrice(ctx)
-	checkErr(err)
+		seqs := []types.Sequence{{
+			GlobalExitRoot: common.HexToHash("0x"),
+			Txs:            []ethtypes.Transaction{},
+			Timestamp:      int64(currentBlock.Time() - 1), // fit in latest-sequence < > current-block rage
+		}}
+		tx, err := ethMan.SequenceBatches(ctx, seqs, 0, nil, nil)
+		checkErr(err)
+		log.Info("TxHash: ", tx.Hash())
 
-	log.Debug("getting nonce")
-	nonce, err := client.PendingNonceAt(ctx, auth.From)
-	checkErr(err)
-
-	tx := ethtypes.NewTransaction(nonce+uint64(1), toAddress, amount, gasLimit, gasPrice, nil)
-	seqs := []types.Sequence{{
-		Txs: []ethtypes.Transaction{*tx},
-	}}
-	_, err = ethMan.SequenceBatches(ctx, seqs, gasLimit*2, gasPrice, big.NewInt(int64(nonce)))
-	checkErr(err)
+		var duration time.Duration
+		if nBatches > 1 {
+			duration = 1
+		}
+		time.Sleep(duration * time.Second)
+	}
+	if nBatches > 1 {
+		log.Infof("Successfully sent %d batches", nBatches)
+	} else {
+		log.Info("Successfully sent 1 batch")
+	}
 }
 
 func checkErr(err error) {
