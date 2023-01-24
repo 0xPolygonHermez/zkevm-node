b12409903fad8781f4993033eee68e239a2711f8
ToniRamirezM <toni@iden3.com>
2023-01-24 15:17:58 +0100

    fix linter

--- test/e2e/broadcast_test.go
+++ test/e2e/broadcast_test.go
@@ -145,7 +145,7 @@ func populateDB(ctx context.Context, st *state.State) error {
 		}
 	}
 
-	const addExitRoots = "INSERT INTO state.exit_root (block_num, timestamp, global_exit_root, mainnet_exit_root, rollup_exit_root) VALUES ($1, $2, $3, $4)"
+	const addExitRoots = "INSERT INTO state.exit_root (block_num, timestamp, global_exit_root, mainnet_exit_root, rollup_exit_root) VALUES ($1, $2, $3, $4, $5)"
 	_, err := st.PostgresStorage.Exec(ctx, addExitRoots, blockNumber, time.Now(), mainnetExitRoot, rollupExitRoot)
 	return err
 }
