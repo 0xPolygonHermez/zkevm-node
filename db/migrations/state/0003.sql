09678c25856af55aa6f02f8869edadcf135942e4
ToniRamirezM <toni@iden3.com>
2023-01-27 17:43:21 +0100

    migrations

--- db/migrations/state/0003.sql
+++ db/migrations/state/0003.sql
@@ -1,5 +1,4 @@
 -- +migrate Up
-<<<<<<< HEAD
 ALTER TABLE state.forced_batch
 DROP COLUMN IF EXISTS batch_num;
 
@@ -8,8 +7,6 @@ ADD COLUMN forced_batch_num BIGINT;
 ALTER TABLE state.batch
 ADD FOREIGN KEY (forced_batch_num) REFERENCES state.forced_batch(forced_batch_num);
 
-=======
->>>>>>> develop
 CREATE TABLE state.debug
 (
     error_type  VARCHAR,
@@ -18,7 +15,6 @@ CREATE TABLE state.debug
 );
 
 -- +migrate Down
-<<<<<<< HEAD
 ALTER TABLE state.batch
 DROP COLUMN IF EXISTS forced_batch_num;
 
@@ -26,6 +22,3 @@ ALTER TABLE state.forced_batch
 ADD COLUMN batch_num BIGINT;
 
 DROP TABLE state.debug;
-=======
-DROP TABLE state.debug;
->>>>>>> develop
--- db/migrations/state/0004.sql
+++ db/migrations/state/0004.sql
@@ -1,15 +1,28 @@
--- +migrate Up
-ALTER TABLE state.forced_batch
-DROP COLUMN IF EXISTS batch_num;
-
-ALTER TABLE state.batch
-ADD COLUMN forced_batch_num BIGINT;
-ALTER TABLE state.batch
-ADD FOREIGN KEY (forced_batch_num) REFERENCES state.forced_batch(forced_batch_num);
-
 -- +migrate Down
-ALTER TABLE state.batch
-DROP COLUMN IF EXISTS forced_batch_num;
+DROP TABLE IF EXISTS state.monitored_txs;
 
-ALTER TABLE state.forced_batch
-ADD COLUMN batch_num BIGINT;
+ALTER TABLE state.verified_batch
+DROP COLUMN IF EXISTS is_trusted;
+
+-- +migrate Up
+CREATE TABLE state.monitored_txs
+(
+    owner      VARCHAR NOT NULL,
+    id         VARCHAR NOT NULL,
+    from_addr  VARCHAR NOT NULL,
+    to_addr    VARCHAR,
+    nonce      DECIMAL(78, 0) NOT NULL,
+    value      DECIMAL(78, 0),
+    data       VARCHAR,
+    gas        DECIMAL(78, 0) NOT NULL,
+    gas_price  DECIMAL(78, 0) NOT NULL,
+    status     VARCHAR NOT NULL,
+    history    VARCHAR[],
+    block_num  BIGINT,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    PRIMARY KEY (owner, id)
+);
+
+ALTER TABLE state.verified_batch
+ADD COLUMN is_trusted BOOLEAN DEFAULT true;
