944deb96437dd6c6a45e24c3f4272432f25f457a
Alonso <ARR551@protonmail.com>
2023-02-10 14:45:43 +0100

    addresses

--- config/default.go
+++ config/default.go
@@ -33,9 +33,9 @@ FreeClaimGasLimit = 150000
 [Etherman]
 URL = "http://localhost:8545"
 L1ChainID = 1337
-PoEAddr = "0x2279B7A0a67DB372996a5FaB50D91eAA73d2eBe6"
+PoEAddr = "0x8A791620dd6260079BF849Dc5567aDC3F2FdC318"
 MaticAddr = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
-GlobalExitRootManagerAddr = "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9"
+GlobalExitRootManagerAddr = "0xa513E6E4b8f2a923D98304ec87F64353C4D5C853"
 MultiGasProvider = true
 	[Etherman.Etherscan]
 		ApiKey = ""
