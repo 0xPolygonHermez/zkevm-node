1b8dfaefb5b5f79f60838391926df1f9edce83f1
tclemos <thiago@iden3.com>
2023-02-11 19:05:50 -0300

    add debug trace By Block Number and Hash

--- jsonrpc/endpoints_debug.go
+++ jsonrpc/endpoints_debug.go
@@ -3,10 +3,14 @@ package jsonrpc
 import (
 	"context"
 	"encoding/hex"
+	"errors"
+	"fmt"
 
 	"github.com/0xPolygonHermez/zkevm-node/log"
+	"github.com/0xPolygonHermez/zkevm-node/state"
 	"github.com/0xPolygonHermez/zkevm-node/state/runtime/instrumentation"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/jackc/pgx/v4"
 )
 
@@ -22,6 +26,10 @@ type traceConfig struct {
 	EnableMemory     bool    `json:"enableMemory"`
 	EnableReturnData bool    `json:"enableReturnData"`
 	Tracer           *string `json:"tracer"`
+}
+
+type traceBlockTransactionResponse struct {
+	Result traceTransactionResponse `json:"result"`
 }
 
 type traceTransactionResponse struct {
@@ -49,54 +57,108 @@ type StructLogRes struct {
 // See https://geth.ethereum.org/docs/interacting-with-geth/rpc/ns-debug#debugtracetransaction
 func (d *DebugEndpoints) TraceTransaction(hash common.Hash, cfg *traceConfig) (interface{}, rpcError) {
 	return d.txMan.NewDbTxScope(d.state, func(ctx context.Context, dbTx pgx.Tx) (interface{}, rpcError) {
-		tracer := ""
-		if cfg != nil && cfg.Tracer != nil {
-			tracer = *cfg.Tracer
-		}
-
-		result, err := d.state.DebugTransaction(ctx, hash, tracer, dbTx)
-		if err != nil {
-			const errorMessage = "failed to debug trace the transaction"
-			log.Infof("%v: %v", errorMessage, err)
-			return nil, newRPCError(defaultErrorCode, errorMessage)
-		}
-
-		if tracer != "" && len(result.ExecutorTraceResult) > 0 {
-			return result.ExecutorTraceResult, nil
-		}
-
-		failed := result.Failed()
-		var returnValue interface{}
-		if cfg.EnableReturnData {
-			returnValue = common.Bytes2Hex(result.ReturnValue)
-		}
-
-		structLogs := buildStructLogs(result.StructLogs, cfg)
-
-		resp := traceTransactionResponse{
-			Gas:         result.GasUsed,
-			Failed:      failed,
-			ReturnValue: returnValue,
-			StructLogs:  structLogs,
-		}
-
-		return resp, nil
+		return d.buildTraceTransaction(ctx, hash, cfg, dbTx)
 	})
 }
 
 // TraceTransaction creates a response for debug_traceBlockByNumber request.
 // See https://geth.ethereum.org/docs/interacting-with-geth/rpc/ns-debug#debugtraceblockbynumber
 func (d *DebugEndpoints) TraceBlockByNumber(number BlockNumber, cfg *traceConfig) (interface{}, rpcError) {
-	return "not implemented yet", nil
+	return d.txMan.NewDbTxScope(d.state, func(ctx context.Context, dbTx pgx.Tx) (interface{}, rpcError) {
+		blockNumber, rpcErr := number.getNumericBlockNumber(ctx, d.state, dbTx)
+		if rpcErr != nil {
+			return nil, rpcErr
+		}
+
+		block, err := d.state.GetL2BlockByNumber(ctx, blockNumber, dbTx)
+		if errors.Is(err, state.ErrNotFound) {
+			return nil, newRPCError(defaultErrorCode, "genesis is not traceable")
+		} else if err == state.ErrNotFound {
+			return rpcErrorResponse(defaultErrorCode, "failed to get block by number", err)
+		}
+
+		traces, rpcErr := d.buildTraceBlock(ctx, block.Transactions(), cfg, dbTx)
+		if err != nil {
+			return nil, rpcErr
+		}
+
+		return traces, nil
+	})
 }
 
 // TraceTransaction creates a response for debug_traceBlockByHash request.
 // See https://geth.ethereum.org/docs/interacting-with-geth/rpc/ns-debug#debugtraceblockbyhash
 func (d *DebugEndpoints) TraceBlockByHash(hash common.Hash, cfg *traceConfig) (interface{}, rpcError) {
-	return "not implemented yet", nil
+	return d.txMan.NewDbTxScope(d.state, func(ctx context.Context, dbTx pgx.Tx) (interface{}, rpcError) {
+		block, err := d.state.GetL2BlockByHash(ctx, hash, dbTx)
+		if errors.Is(err, state.ErrNotFound) {
+			return nil, newRPCError(defaultErrorCode, "genesis is not traceable")
+		} else if err == state.ErrNotFound {
+			return rpcErrorResponse(defaultErrorCode, "failed to get block by hash", err)
+		}
+
+		traces, rpcErr := d.buildTraceBlock(ctx, block.Transactions(), cfg, dbTx)
+		if err != nil {
+			return nil, rpcErr
+		}
+
+		return traces, nil
+	})
 }
 
-func buildStructLogs(stateStructLogs []instrumentation.StructLog, cfg *traceConfig) []StructLogRes {
+func (d *DebugEndpoints) buildTraceBlock(ctx context.Context, txs []*types.Transaction, cfg *traceConfig, dbTx pgx.Tx) (interface{}, rpcError) {
+	traces := []traceBlockTransactionResponse{}
+	for _, tx := range txs {
+		traceTransaction, err := d.buildTraceTransaction(ctx, tx.Hash(), cfg, dbTx)
+		if err != nil {
+			errMsg := fmt.Sprintf("failed to get trace for transaction %v", tx.Hash().String())
+			return rpcErrorResponse(defaultErrorCode, errMsg, err)
+		}
+		traceBlockTransaction := traceBlockTransactionResponse{
+			Result: traceTransaction.(traceTransactionResponse),
+		}
+		traces = append(traces, traceBlockTransaction)
+	}
+
+	return traces, nil
+}
+
+func (d *DebugEndpoints) buildTraceTransaction(ctx context.Context, hash common.Hash, cfg *traceConfig, dbTx pgx.Tx) (interface{}, rpcError) {
+	tracer := ""
+	if cfg != nil && cfg.Tracer != nil {
+		tracer = *cfg.Tracer
+	}
+
+	result, err := d.state.DebugTransaction(ctx, hash, tracer, dbTx)
+	if err != nil {
+		const errorMessage = "failed to get trace"
+		log.Infof("%v: %v", errorMessage, err)
+		return nil, newRPCError(defaultErrorCode, errorMessage)
+	}
+
+	if tracer != "" && len(result.ExecutorTraceResult) > 0 {
+		return result.ExecutorTraceResult, nil
+	}
+
+	failed := result.Failed()
+	var returnValue interface{}
+	if cfg.EnableReturnData {
+		returnValue = common.Bytes2Hex(result.ReturnValue)
+	}
+
+	structLogs := d.buildStructLogs(result.StructLogs, cfg)
+
+	resp := traceTransactionResponse{
+		Gas:         result.GasUsed,
+		Failed:      failed,
+		ReturnValue: returnValue,
+		StructLogs:  structLogs,
+	}
+
+	return resp, nil
+}
+
+func (d *DebugEndpoints) buildStructLogs(stateStructLogs []instrumentation.StructLog, cfg *traceConfig) []StructLogRes {
 	structLogs := make([]StructLogRes, 0, len(stateStructLogs))
 	for _, structLog := range stateStructLogs {
 
