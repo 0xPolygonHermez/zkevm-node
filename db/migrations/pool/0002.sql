0692dab1462a79d96912b7c8abc3fef551eaae9a
ToniRamirezM <toni@iden3.com>
2023-02-20 17:40:58 +0100

    fix migration

--- db/migrations/pool/0002.sql
+++ db/migrations/pool/0002.sql
@@ -1,5 +1,5 @@
--- +migrate Down
+-- +migrate Up
 ALTER TABLE pool.transaction ADD COLUMN preprocessed_state_root varchar;
 
--- +migrate Up
+-- +migrate Down
 ALTER TABLE pool.transaction DROP COLUMN preprocessed_state_root;
