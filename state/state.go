e1c00dab248480fdef8e7edd6b48015044c04b17
Thiago Coimbra Lemos <tclemos@users.noreply.github.com>
2023-03-06 14:05:18 -0300

    allow reverted tx to be added to the pool (#1744)

--- state/state.go
+++ state/state.go
@@ -1182,7 +1182,12 @@ func (s *State) PreProcessTransaction(ctx context.Context, tx *types.Transaction
 		return nil, err
 	}
 
-	return s.internalProcessUnsignedTransaction(ctx, tx, sender, &lastL2BlockNumber, false, &nonce, dbTx)
+	response, err := s.internalProcessUnsignedTransaction(ctx, tx, sender, &lastL2BlockNumber, false, &nonce, dbTx)
+	if err != nil && !errors.Is(err, runtime.ErrExecutionReverted) {
+		return nil, err
+	}
+
+	return response, nil
 }
 
 // ProcessUnsignedTransaction processes the given unsigned transaction.
