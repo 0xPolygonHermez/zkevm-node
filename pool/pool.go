e1d10ea7999e0e60086f454c0ae9702cf5645608
Alonso <ARR551@protonmail.com>
2023-03-08 16:55:27 +0100

    linter

--- pool/pool.go
+++ pool/pool.go
@@ -7,8 +7,8 @@ import (
 	"math/big"
 	"time"
 
-	"github.com/0xPolygonHermez/zkevm-node/state"
 	"github.com/0xPolygonHermez/zkevm-node/log"
+	"github.com/0xPolygonHermez/zkevm-node/state"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/math"
 	"github.com/ethereum/go-ethereum/core/types"
