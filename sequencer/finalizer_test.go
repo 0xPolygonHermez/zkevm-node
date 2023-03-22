e8c4cbbe7b132a8487f463e4d2c8161741ee9ff1
ToniRamirezM <toni@iden3.com>
2023-03-22 13:32:58 +0100

    fix test

--- sequencer/finalizer_test.go
+++ sequencer/finalizer_test.go
@@ -945,9 +945,9 @@ func TestFinalizer_checkRemainingResources(t *testing.T) {
 		t.Run(tc.name, func(t *testing.T) {
 			// arrange
 			f.batch.remainingResources = tc.remaining
+			workerMock.On("AddEvent", ctx, mock.Anything, nil).Return(nil)
 			if tc.expectedWorkerUpdate {
 				workerMock.On("UpdateTx", txResponse.TxHash, tc.expectedTxTracker.From, result.UsedZkCounters).Return().Once()
-				workerMock.On("AddEvent", ctx, mock.Anything, nil).Return(nil)
 			}
 
 			// act
