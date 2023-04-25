8d2a927583dd5f6c51d6d25eca9bc961ef3d9ebc
Thiago Lemos <thiago@polygon.technology>
2023-04-25 14:06:15 -0300

    fix query

--- pool/pgpoolstorage/pgpoolstorage.go
+++ pool/pgpoolstorage/pgpoolstorage.go
@@ -73,7 +73,7 @@ func (p *PostgresPoolStorage) AddTx(ctx context.Context, tx pool.Transaction) er
 			received_at,
 			from_address,
 			is_wip,
-			ip,
+			ip
 		) 
 		VALUES 
 			($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18)
