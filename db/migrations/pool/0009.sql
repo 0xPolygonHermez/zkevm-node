74aa1618742c90caddb64cfec6dbcba69924b90d
najeal <haim.nathan@icloud.com>
2023-05-16 17:50:17 +0200

    migration down: delete index only if exists

--- db/migrations/pool/0009.sql
+++ db/migrations/pool/0009.sql
@@ -2,4 +2,4 @@
 CREATE INDEX idx_pool_gas_price_timestamp ON pool.gas_price (timestamp);
 
 -- +migrate Down
-DROP INDEX idx_pool_gas_price_timestamp; 
+DROP INDEX IF EXISTS idx_pool_gas_price_timestamp; 
