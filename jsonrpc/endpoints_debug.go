67edd849aa2887c5b4b70f0954c4e49be77734ed
tclemos <thiago@polygon.technology>
2023-06-22 17:08:28 -0300

    adds support to debug_traceBatchByNumber

--- jsonrpc/endpoints_debug.go
+++ jsonrpc/endpoints_debug.go
@@ -6,8 +6,13 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"net/http"
+	"net/url"
 	"strings"
+	"sync"
+	"time"
 
+	"github.com/0xPolygonHermez/zkevm-node/jsonrpc/client"
 	"github.com/0xPolygonHermez/zkevm-node/jsonrpc/types"
 	"github.com/0xPolygonHermez/zkevm-node/log"
 	"github.com/0xPolygonHermez/zkevm-node/state"
@@ -66,6 +71,11 @@ type traceBlockTransactionResponse struct {
 	Result interface{} `json:"result"`
 }
 
+type traceBatchTransactionResponse struct {
+	TxHash common.Hash `json:"txHash"`
+	Result interface{} `json:"result"`
+}
+
 // TraceTransaction creates a response for debug_traceTransaction request.
 // See https://geth.ethereum.org/docs/interacting-with-geth/rpc/ns-debug#debugtracetransaction
 func (d *DebugEndpoints) TraceTransaction(hash types.ArgHash, cfg *traceConfig) (interface{}, types.Error) {
@@ -115,6 +125,131 @@ func (d *DebugEndpoints) TraceBlockByHash(hash types.ArgHash, cfg *traceConfig) 
 			return nil, rpcErr
 		}
 
+		return traces, nil
+	})
+}
+
+// TraceBatchByNumber creates a response for debug_traceBatchByNumber request.
+// this endpoint tries to help clients to get traces at once for all the transactions
+// attached to the same batch.
+//
+// IMPORTANT: in order to take advantage of the infrastructure automatically scaling,
+// instead of parallelizing the trace transaction internally and pushing all the load
+// to a single jRPC and Executor instance, the code will redirect the trace transaction
+// requests to the same url, making them external calls, so we can process in parallel
+// with multiple jRPC and Executor instances.
+//
+// the request flow will work as follows:
+// -> user do a trace batch request
+// -> jRPC balancer picks a jRPC server to handle the trace batch request
+// -> picked jRPC sends parallel trace transaction requests for each transaction in the batch
+// -> jRPC balancer sends each request to a different jRPC to handle the trace transaction requests
+// -> picked jRPC server group trace transaction responses from other jRPC servers
+// -> picked jRPC respond the initial request to the user with all the tx traces
+func (d *DebugEndpoints) TraceBatchByNumber(httpRequest *http.Request, number types.BatchNumber, cfg *traceConfig) (interface{}, types.Error) {
+	// timeout is the maximum time the code will wait for all the
+	// traces to be loaded from the jRPC and added to the responses
+	const timeout = 10 * time.Minute
+
+	// the size of the buffer defines
+	// how many txs it will process in parallel.
+	const bufferSize = 10
+
+	// builds the url of the jRPC server from the data found in the httpRequest
+	scheme := "http"
+	if httpRequest.URL.Scheme != "" {
+		scheme = httpRequest.URL.Scheme
+	}
+	u := url.URL{
+		Scheme: scheme,
+		Host:   httpRequest.Host,
+		Path:   httpRequest.URL.Path,
+	}
+	rpcURL := u.String()
+
+	return d.txMan.NewDbTxScope(d.state, func(ctx context.Context, dbTx pgx.Tx) (interface{}, types.Error) {
+		batchNumber, rpcErr := number.GetNumericBatchNumber(ctx, d.state, dbTx)
+		if rpcErr != nil {
+			return nil, rpcErr
+		}
+
+		batch, err := d.state.GetBatchByNumber(ctx, batchNumber, dbTx)
+		if errors.Is(err, state.ErrNotFound) {
+			return nil, types.NewRPCError(types.DefaultErrorCode, fmt.Sprintf("batch #%d not found", batchNumber))
+		} else if err == state.ErrNotFound {
+			return rpcErrorResponse(types.DefaultErrorCode, "failed to get batch by number", err)
+		}
+
+		txs, err := d.state.GetTransactionsByBatchNumber(ctx, batch.BatchNumber, dbTx)
+		if !errors.Is(err, state.ErrNotFound) && err != nil {
+			return rpcErrorResponse(types.DefaultErrorCode, fmt.Sprintf("couldn't load batch txs from state by number %v to create the traces", batchNumber), err)
+		}
+
+		receipts := make([]ethTypes.Receipt, 0, len(txs))
+		for _, tx := range txs {
+			receipt, err := d.state.GetTransactionReceipt(ctx, tx.Hash(), dbTx)
+			if err != nil {
+				return rpcErrorResponse(types.DefaultErrorCode, fmt.Sprintf("couldn't load receipt for tx %v to get trace", tx.Hash().String()), err)
+			}
+			receipts = append(receipts, *receipt)
+		}
+
+		requests := make(chan (ethTypes.Receipt), bufferSize)
+
+		wg := sync.WaitGroup{}
+		wg.Add(len(receipts))
+		responses := make(chan (traceBatchTransactionResponse), len(receipts))
+
+		// gets the trace from the jRPC and adds it to the responses
+		loadTraceByTxHash := func(receipt ethTypes.Receipt) {
+			defer wg.Done()
+			res, err := client.JSONRPCCall(rpcURL, "debug_traceTransaction", receipt.TxHash.String(), cfg)
+			if err != nil {
+				log.Errorf("failed to get tx trace from remote jRPC server %v, err: %v", rpcURL, err)
+				return
+			}
+
+			if res.Error != nil {
+				log.Errorf("tx trace error returned from remote jRPC server %v, %v %v", rpcURL, res.Error.Code, res.Error.Message)
+				return
+			}
+
+			// add to the responses
+			responses <- traceBatchTransactionResponse{
+				TxHash: receipt.TxHash,
+				Result: res.Result,
+			}
+		}
+
+		// goes through the buffer and loads the trace
+		// by all the transactions added in the buffer
+		// then add the results to the responses map
+		go func() {
+			index := uint(0)
+			for req := range requests {
+				go loadTraceByTxHash(req)
+				index++
+			}
+		}()
+
+		// add receipts to the buffer
+		for _, receipt := range receipts {
+			requests <- receipt
+		}
+
+		// wait the traces to be loaded
+		if waitTimeout(&wg, timeout) {
+			return rpcErrorResponse(types.DefaultErrorCode, fmt.Sprintf("failed to get traces for batch %v: timeout reached", batchNumber), nil)
+		}
+
+		close(requests)
+		close(responses)
+
+		// build the batch trace response array
+		traces := make([]traceBatchTransactionResponse, 0, len(receipts))
+		for response := range responses {
+			traces = append(traces, response)
+		}
 		return traces, nil
 	})
 }
@@ -292,4 +427,20 @@ func isBuiltInTracer(tracer string) bool {
 // https://geth.ethereum.org/docs/developers/evm-tracing/custom-tracer
 func isJSCustomTracer(tracer string) bool {
 	return strings.Contains(tracer, "result") && strings.Contains(tracer, "fault")
+}
+
+// waitTimeout waits for the waitGroup for the specified max timeout.
+// Returns true if waiting timed out.
+func waitTimeout(wg *sync.WaitGroup, timeout time.Duration) bool {
+	c := make(chan struct{})
+	go func() {
+		defer close(c)
+		wg.Wait()
+	}()
+	select {
+	case <-c:
+		return false // completed normally
+	case <-time.After(timeout):
+		return true // timed out
+	}
 }
