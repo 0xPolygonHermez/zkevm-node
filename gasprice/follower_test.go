10df9bf2c66da137f594c0544ce89868c64f4d2e
Toni Ramírez <toni@polygon.technology>
2023-07-18 10:26:58 +0200

    add test

--- gasprice/follower_test.go
+++ gasprice/follower_test.go
@@ -39,3 +39,24 @@ func TestUpdateGasPriceFollower(t *testing.T) {
 	poolM.On("SetGasPrices", ctx, l2GasPrice, l1GasPrice.Uint64()).Return(nil).Once()
 	f.UpdateGasPriceAvg()
 }
+
+func TestLimitMasGasPrice(t *testing.T) {
+	ctx := context.Background()
+	var d time.Duration = 1000000000
+
+	cfg := Config{
+		Type:               FollowerType,
+		DefaultGasPriceWei: 100000000,
+		MaxGasPriceWei:     50000000,
+		UpdatePeriod:       types.NewDuration(d),
+		Factor:             0.5,
+	}
+	l1GasPrice := big.NewInt(1000000000)
+	poolM := new(poolMock)
+	ethM := new(ethermanMock)
+	ethM.On("GetL1GasPrice", ctx).Return(l1GasPrice)
+	// Ensure SetGasPrices is called with the MaxGasPriceWei
+	poolM.On("SetGasPrices", ctx, cfg.MaxGasPriceWei, l1GasPrice.Uint64()).Return(nil)
+	f := newFollowerGasPriceSuggester(ctx, cfg, poolM, ethM)
+	f.UpdateGasPriceAvg()
+}
