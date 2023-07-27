7c459dd73ad8d8edda08b7240576758d14cc2ee6
kladkogex <13399135+kladkogex@users.noreply.github.com>
2023-07-27 22:43:20 +0100

    Pull candidate blocks from decentralized sequencer

--- jsonrpc/candidate_block_pull_agent_levitation.go
+++ jsonrpc/candidate_block_pull_agent_levitation.go
@@ -2,11 +2,15 @@
 
 package jsonrpc
 
+import (
+	"time"
+)
+
 // CandidateBlockPullAgent is pull candidate blocks from decentralized sequencer
 type CandidateBlockPullAgent struct {
-	config  Config
-	chainID uint64
-	handler *Handler
+	config        Config
+	chainID       uint64
+	exitRequested bool
 }
 
 // NewCandidateBlockPullAgent returns the CandidateBlockPullAgent
@@ -15,18 +19,28 @@ func NewCandidateBlockPullAgent(
 	chainID uint64,
 ) *CandidateBlockPullAgent {
 	agent := &CandidateBlockPullAgent{
-		config:  cfg,
-		chainID: chainID,
+		config:        cfg,
+		chainID:       chainID,
+		exitRequested: false,
 	}
+
 	return agent
 }
 
 // Start initializes the JSON RPC server to listen for request
 func (agent *CandidateBlockPullAgent) Start() error {
+	go agent.doWork()
 	return nil
 }
 
 // Stop shutdown the rpc server
 func (agent *CandidateBlockPullAgent) Stop() error {
+	agent.exitRequested = true
 	return nil
+}
+
+func (agent *CandidateBlockPullAgent) doWork() {
+	for !agent.exitRequested {
+		time.Sleep(1 * time.Second)
+	}
 }
