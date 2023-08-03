e16f7646187fca2fcafeae57356e9a8375378a23
joanestebanr <129153821+joanestebanr@users.noreply.github.com>
2023-08-03 14:46:51 +0200

    create a index for state.receipt.block_num

---
+++ db/migrations/state/0008.sql
@@ -0,0 +1,6 @@
+-- #2324 speed up delete batches
+-- +migrate Up
+CREATE INDEX IF NOT EXISTS receipt_block_num_idx ON state.receipt USING btree (block_num);
+
+-- +migrate Down
+DROP INDEX IF EXISTS receipt_block_num_idx;
