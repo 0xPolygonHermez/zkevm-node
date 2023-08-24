2d3837f93c5a2fe6c11c944d0e4d0d2ba05a3031
Toni Ramírez <toni@polygon.technology>
2023-08-24 10:00:13 +0200

    receipt deletion index

--- db/migrations/state/0008_test.go
+++ db/migrations/state/0008_test.go
@@ -15,7 +15,7 @@ var indexes_0008 = []string{"receipt_block_num_idx"}
 func (m migrationTest0008) InsertData(db *sql.DB) error {
 	const insertBatch = `
 		INSERT INTO state.batch (batch_num, global_exit_root, local_exit_root, acc_input_hash, state_root, timestamp, coinbase, raw_txs_data, forced_batch_num) 
-		                 VALUES (        0,          '0x000',         '0x000',        '0x000',    '0x000',     now(),  '0x000',         null,             null)`
+		VALUES (0,'0x000', '0x000', '0x000', '0x000', now(), '0x000', null, null)`
 
 	// insert batch
 	_, err := db.Exec(insertBatch)
@@ -24,8 +24,8 @@ func (m migrationTest0008) InsertData(db *sql.DB) error {
 	}
 
 	const insertL2Block = `
-	INSERT INTO state.l2block (block_num, block_hash, header, uncles, parent_hash, state_root, received_at, batch_num, created_at)
-					   VALUES (       0,     '0x001',   '{}',   '{}',      '0x002',   '0x003',       now(),         0,      now())`
+		INSERT INTO state.l2block (block_num, block_hash, header, uncles, parent_hash, state_root, received_at, batch_num, created_at)
+		VALUES (0, '0x001', '{}', '{}', '0x002', '0x003', now(), 0, now())`
 
 	// insert l2 block
 	_, err = db.Exec(insertL2Block)
@@ -34,8 +34,8 @@ func (m migrationTest0008) InsertData(db *sql.DB) error {
 	}
 
 	const insertTx = `
-		INSERT INTO state.transaction (   hash,  encoded, decoded, l2_block_num, effective_percentage)
-		 				       VALUES ('0x001', 'ABCDEF',    '{}',            0,                  255)`
+		INSERT INTO state.transaction (hash, encoded, decoded, l2_block_num, effective_percentage)
+		VALUES ('0x001', 'ABCDEF', '{}', 0, 255)`
 
 	// insert tx
 	_, err = db.Exec(insertTx)
@@ -43,8 +43,8 @@ func (m migrationTest0008) InsertData(db *sql.DB) error {
 		return err
 	}
 	const insertReceipt = `
-        INSERT INTO state.receipt (tx_hash, type, post_state, status, cumulative_gas_used, gas_used, effective_gas_price, block_num, tx_index, contract_address)
-                           VALUES ('0x001',    1,       null,      1,                1234,     1234,        		   1,         0,        0,		    '0x002')`
+		INSERT INTO state.receipt (tx_hash, type, post_state, status, cumulative_gas_used, gas_used, effective_gas_price, block_num, tx_index, contract_address)
+		VALUES ('0x001', 1, null, 1, 1234, 1234, 1, 0, 0, '0x002')`
 
 	// insert receipt
 	_, err = db.Exec(insertReceipt)
