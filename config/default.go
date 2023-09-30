e01de402fe676e5e933e93fb0a785497a16d4d10
tclemos <thiago@polygon.technology>
2023-09-30 12:24:07 -0300

    remove deprecated TraceBatchUseHTTPS config

--- config/default.go
+++ config/default.go
@@ -75,7 +75,6 @@ WriteTimeout = "60s"
 MaxRequestsPerIPAndSecond = 500
 SequencerNodeURI = ""
 EnableL2SuggestedGasPricePolling = true
-TraceBatchUseHTTPS = true
 BatchRequestsEnabled = false
 BatchRequestsLimit = 20
 	[RPC.WebSockets]
