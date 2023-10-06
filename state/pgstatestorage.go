88cd7a3222bd6421fedabb1181c56b0ca9206f2d
Alonso Rodriguez <ARR552@users.noreply.github.com>
2023-10-06 11:58:53 +0200

    migration + tests and queries (#2625) (#2626)

--- state/pgstatestorage.go
+++ state/pgstatestorage.go
@@ -542,8 +542,7 @@ func (p *PostgresStorage) GetLatestVirtualBatchTimestamp(ctx context.Context, db
 func (p *PostgresStorage) SetLastBatchInfoSeenOnEthereum(ctx context.Context, lastBatchNumberSeen, lastBatchNumberVerified uint64, dbTx pgx.Tx) error {
 	const query = `
     UPDATE state.sync_info
-       SET last_batch_num_seen = $1
-         , last_batch_num_consolidated = $2`
+       SET last_batch_num_seen = $1, last_batch_num_consolidated = $2`
 
 	e := p.getExecQuerier(dbTx)
 	_, err := e.Exec(ctx, query, lastBatchNumberSeen, lastBatchNumberVerified)
@@ -2159,7 +2158,7 @@ func (p *PostgresStorage) GetExitRootByGlobalExitRoot(ctx context.Context, ger c
 
 // AddSequence stores the sequence information to allow the aggregator verify sequences.
 func (p *PostgresStorage) AddSequence(ctx context.Context, sequence Sequence, dbTx pgx.Tx) error {
-	const addSequenceSQL = "INSERT INTO state.sequences (from_batch_num, to_batch_num) VALUES($1, $2)"
+	const addSequenceSQL = "INSERT INTO state.sequences (from_batch_num, to_batch_num) VALUES($1, $2) ON CONFLICT (from_batch_num) DO UPDATE SET to_batch_num = $2"
 
 	e := p.getExecQuerier(dbTx)
 	_, err := e.Exec(ctx, addSequenceSQL, sequence.FromBatchNumber, sequence.ToBatchNumber)
