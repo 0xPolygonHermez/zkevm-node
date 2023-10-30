bf018dcd727444ad816da7c891a4f5de2a519d01
Toni Ramírez <toni@polygon.technology>
2023-10-30 12:37:59 +0100

    add pool indexes

--- db/migrations/pool/0011.sql
+++ db/migrations/pool/0011.sql
@@ -3,9 +3,7 @@ CREATE TABLE pool.whitelisted (
 	addr VARCHAR PRIMARY KEY
 );
 
-CREATE INDEX IF NOT EXISTS idx_transaction_from ON pool.transaction (from_address);
 CREATE INDEX IF NOT EXISTS idx_transaction_from_nonce ON pool.transaction (from_address, nonce);
-CREATE INDEX IF NOT EXISTS idx_transaction_status ON pool.transaction (status);
 CREATE INDEX IF NOT EXISTS idx_transaction_wip_status ON pool.transaction (is_wip, status);
 CREATE INDEX IF NOT EXISTS idx_transaction_status_gas_price ON pool.transaction (status, gas_price);
 CREATE INDEX IF NOT EXISTS idx_transaction_status_received_at ON pool.transaction (status, idx_transaction_status_received_at);
@@ -15,9 +13,7 @@ CREATE INDEX IF NOT EXISTS idx_transaction_hash ON pool.transaction (hash);
 -- +migrate Down
 DROP TABLE pool.whitelisted;
 
-DROP INDEX IF EXISTS pool.idx_transaction_from
 DROP INDEX IF EXISTS pool.idx_transaction_from_nonce
-DROP INDEX IF EXISTS pool.idx_transaction_status
 DROP INDEX IF EXISTS pool.idx_transaction_wip_status
 DROP INDEX IF EXISTS pool.idx_transaction_status_gas_price
 DROP INDEX IF EXISTS pool.idx_transaction_status_received_at
