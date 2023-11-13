d0782da6db43ee4177b2edc0e3bf428411dcc50a
Alonso <ARR551@protonmail.com>
2023-11-13 13:19:51 +0100

    implementation + tests

---
+++ l1infotree/tree.go
@@ -0,0 +1,58 @@
+package l1infotree
+
+import (
+	"fmt"
+
+	"github.com/ethereum/go-ethereum/common"
+)
+
+// L1InfoTree provides methods to compute L1InfoTree
+type L1InfoTree struct {
+	height uint8
+	zeroHashes [][32]byte
+}
+
+// NewL1InfoTree creates new L1InfoTree.
+func NewL1InfoTree(height uint8) *L1InfoTree {
+	return &L1InfoTree{
+		zeroHashes: generateZeroHashes(height),
+		height: height,
+	}
+}
+
+func buildIntermediate(leaves [][32]byte) ([][][]byte, [][32]byte) {
+	var (
+		nodes [][][]byte
+		hashes [][32]byte
+	)
+	for i := 0; i < len(leaves); i += 2 {
+		var left, right int = i, i + 1
+		hash := Hash(leaves[left], leaves[right])
+		nodes = append(nodes, [][]byte{hash[:], leaves[left][:], leaves[right][:]})
+		hashes = append(hashes, hash)
+	}
+	return nodes, hashes
+}
+
+func (mt *L1InfoTree) BuildL1InfoRoot(leaves [][32]byte) (common.Hash, error) {
+	var (
+		nodes [][][][]byte
+		ns [][][]byte
+	)
+	if len(leaves) == 0 {
+		leaves = append(leaves, mt.zeroHashes[0])
+	}
+
+	for h := uint8(0); h < mt.height; h++ {
+		if len(leaves)%2 == 1 {
+			leaves = append(leaves, mt.zeroHashes[h])
+		}
+		ns, leaves = buildIntermediate(leaves)
+		nodes = append(nodes, ns)
+	}
+	if len(ns) != 1 {
+		return common.Hash{}, fmt.Errorf("error: more than one root detected: %+v", nodes)
+	}
+	
+	return common.BytesToHash(ns[0][0]), nil
+}
---
+++ l1infotree/tree_test.go
@@ -0,0 +1,39 @@
+package l1infotree
+
+import (
+	"encoding/json"
+	"os"
+	"testing"
+
+	"github.com/0xPolygonHermez/zkevm-node/test/vectors"
+	"github.com/stretchr/testify/require"
+)
+
+func TestComputeTreeRoot(t *testing.T) {
+	data, err := os.ReadFile("../test/vectors/src/merkle-tree/l1-info-tree/root-vectors.json")
+	require.NoError(t, err)
+	var mtTestVectors []vectors.L1InfoTree
+	err = json.Unmarshal(data, &mtTestVectors)
+	require.NoError(t, err)
+	for _, testVector := range mtTestVectors {
+		input := testVector.PreviousLeafValues
+		mt := NewL1InfoTree(uint8(32))
+		require.NoError(t, err)
+
+		var leaves [][32]byte
+		for _, v := range input {
+			leaves = append(leaves, v)
+		}
+
+		if len(leaves) != 0 {
+			root, err := mt.BuildL1InfoRoot(leaves)
+			require.NoError(t, err)
+			require.Equal(t, testVector.CurrentRoot, root)	
+		}
+
+		leaves = append(leaves, testVector.NewLeafValue)
+		newRoot, err := mt.BuildL1InfoRoot(leaves)
+		require.NoError(t, err)
+		require.Equal(t, testVector.NewRoot, newRoot)
+	}
+}
