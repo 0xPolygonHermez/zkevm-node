18a458cf4d35ebe11deee4db11fca203d11fc2d1
Toni Ramírez <58293609+ToniRamirezM@users.noreply.github.com>
2023-11-15 18:06:38 +0100

    Update GER on closed batch without txs (#2793)
    * update GER on closed batch without txs
    
    * refactor

--- sequencer/finalizer_test.go
+++ sequencer/finalizer_test.go
@@ -2545,7 +2545,7 @@ func setupFinalizer(withWipBatch bool) *finalizer {
 		batchConstraints:   bc,
 		processRequest:     state.ProcessRequest{},
 		sharedResourcesMux: new(sync.RWMutex),
-		lastGERHash:        common.Hash{},
+		currentGERHash:     common.Hash{},
 		// closing signals
 		nextGER:                      common.Hash{},
 		nextGERDeadline:              0,
