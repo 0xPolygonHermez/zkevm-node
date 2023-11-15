91096e95cc06b4d904da79f98745803ca78cc5cd
Alonso <ARR551@protonmail.com>
2023-11-15 11:21:39 +0100

    forkID 6

--- test/e2e/shared.go
+++ test/e2e/shared.go
@@ -27,7 +27,7 @@ const (
 	invalidParamsErrorCode = -32602
 	toAddressHex           = "0x4d5Cf5032B2a844602278b01199ED191A86c93ff"
 	gerFinalityBlocks      = uint64(250)
-	forkID                = 5
+	forkID                 = 6
 )
 
 var (
