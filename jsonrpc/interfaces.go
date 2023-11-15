491060f34d85a754569d4d8ada25e4c712cbde57
tclemos <thiago@polygon.technology>
2023-11-15 16:36:40 -0300

    expose mutex and log filters from storage to allow direct access avoiding the copy

--- jsonrpc/interfaces.go
+++ jsonrpc/interfaces.go
@@ -2,8 +2,10 @@ package jsonrpc
 
 // storageInterface json rpc internal storage to persist data
 type storageInterface interface {
-	GetAllBlockFiltersWithWSConn() ([]*Filter, error)
-	GetAllLogFiltersWithWSConn() ([]*Filter, error)
+	Lock()
+	Unlock()
+	GetAllBlockFiltersWithWSConn() map[string]*Filter
+	GetAllLogFiltersWithWSConn() map[string]*Filter
 	GetFilter(filterID string) (*Filter, error)
 	NewBlockFilter(wsConn *concurrentWsConn) (string, error)
 	NewLogFilter(wsConn *concurrentWsConn, filter LogFilter) (string, error)
