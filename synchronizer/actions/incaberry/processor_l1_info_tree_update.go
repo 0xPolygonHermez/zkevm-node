7e08180c4519e8751fbc78c1801590f587a875b0
joanestebanr <129153821+joanestebanr@users.noreply.github.com>
2023-11-23 18:05:53 +0100

    support to event L1InfoTree for incabeery

--- synchronizer/actions/incaberry/processor_l1_info_tree_update.go
+++ synchronizer/actions/incaberry/processor_l1_info_tree_update.go
@@ -1,4 +1,4 @@
-package etrog
+package incaberry
 
 import (
 	"context"
@@ -12,7 +12,7 @@ import (
 
 // stateProcessorL1InfoTreeInterface interface required from state
 type stateProcessorL1InfoTreeInterface interface {
-	AddL1InfoTreeLeaf(ctx context.Context, L1InfoTreeLeaf *state.L1InfoTreeLeaf, dbTx pgx.Tx) (*state.L1InfoTreeExitRootStorageEntry, error)
+	AddGlobalExitRoot(ctx context.Context, exitRoot *state.GlobalExitRoot, dbTx pgx.Tx) error
 }
 
 // ProcessorL1InfoTreeUpdate implements L1EventProcessor for GlobalExitRootsOrder
@@ -26,7 +26,7 @@ func NewProcessorL1InfoTreeUpdate(state stateProcessorL1InfoTreeInterface) *Proc
 	return &ProcessorL1InfoTreeUpdate{
 		ProcessorBase: actions.ProcessorBase[ProcessorL1InfoTreeUpdate]{
 			SupportedEvent:    []etherman.EventOrder{etherman.L1InfoTreeOrder},
-			SupportedForkdIds: &ForksIdOnlyEtrog},
+			SupportedForkdIds: &actions.ForksIdToIncaberry},
 		state: state}
 }
 
@@ -40,15 +40,17 @@ func (p *ProcessorL1InfoTreeUpdate) Process(ctx context.Context, order etherman.
 		GlobalExitRoot:  l1InfoTree.GlobalExitRoot.GlobalExitRoot,
 		Timestamp:       l1InfoTree.MinTimestamp,
 	}
-	l1IntoTreeLeaf := state.L1InfoTreeLeaf{
-		GlobalExitRoot:    ger,
-		PreviousBlockHash: l1InfoTree.PreviousBlockHash,
-	}
-	entry, err := p.state.AddL1InfoTreeLeaf(ctx, &l1IntoTreeLeaf, dbTx)
+
+	err := p.state.AddGlobalExitRoot(ctx, &ger, dbTx)
 	if err != nil {
-		log.Errorf("error storing the l1InfoTree(etrog). BlockNumber: %d, error: %v", l1Block.BlockNumber, err)
+		log.Errorf("error storing the L1InfoTreeOrder(incaberry). BlockNumber: %d, error: %v", l1Block.BlockNumber, err)
+		rollbackErr := dbTx.Rollback(ctx)
+		if rollbackErr != nil {
+			log.Errorf("error rolling back state. BlockNumber: %d, rollbackErr: %s, error : %v", l1Block.BlockNumber, rollbackErr.Error(), err)
+			return rollbackErr
+		}
+		log.Errorf("error storing the L1InfoTreeOrder(incaberry). BlockNumber: %d, error: %v", l1Block.BlockNumber, err)
 		return err
 	}
-	log.Infof("L1InfoTree(etrog) stored. BlockNumber: %d,GER:%s L1InfoTreeIndex: %d L1InfoRoot:%s", l1Block.BlockNumber, entry.GlobalExitRoot.GlobalExitRoot, entry.L1InfoTreeIndex, entry.L1InfoTreeRoot)
 	return nil
 }
