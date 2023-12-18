b56b91c20914bdb296167823edcf79775f564313
Toni Ramírez <58293609+ToniRamirezM@users.noreply.github.com>
2023-12-18 13:23:43 +0100

    New state test vectors (#2933)
    * new state test vectors
    
    * remove log

--- state/test/forkid_etrog/etrog_test.go
+++ state/test/forkid_etrog/etrog_test.go
@@ -16,6 +16,10 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+const (
+	testsFolder = "../../../test/vectors/src/etrog/"
+)
+
 var (
 	testState *state.State
 	forkID    = uint64(state.FORKID_ETROG)
@@ -40,83 +44,92 @@ func TestMain(m *testing.M) {
 	os.Exit(result)
 }
 
-// TestStateTransition tests state transitions using the vector
+// TestStateTransition tests state using test vectors
 func TestStateTransition(t *testing.T) {
 	ctx := context.Background()
-	// Load test vectors
-	testCases, err := vectors.LoadStateTransitionTestCasesEtrog("../../../test/vectors/src/state-transition/etrog/balances.json")
+
+	// Get all tests vector in the etrog folder
+	files, err := os.ReadDir(testsFolder)
 	require.NoError(t, err)
 
-	// Run test cases
-	for i, testCase := range testCases {
-		block := state.Block{
-			BlockNumber: uint64(i + 1),
-			BlockHash:   state.ZeroHash,
-			ParentHash:  state.ZeroHash,
-			ReceivedAt:  time.Now(),
-		}
-
-		genesisActions := vectors.GenerateGenesisActionsEtrog(testCase.Genesis)
-
-		dbTx, err := testState.BeginStateTransaction(ctx)
+	for _, file := range files {
+		// Load test vectors
+		testCases, err := vectors.LoadStateTransitionTestCasesEtrog(testsFolder + file.Name())
 		require.NoError(t, err)
 
-		stateRoot, err := testState.SetGenesis(ctx, block, state.Genesis{Actions: genesisActions}, metrics.SynchronizerCallerLabel, dbTx)
-		require.NoError(t, err)
-		require.Equal(t, testCase.ExpectedOldStateRoot, stateRoot.String())
-		err = dbTx.Rollback(ctx)
-		require.NoError(t, err)
-
-		// convert vector txs
-		txs := make([]state.L2TxRaw, 0, len(testCase.Txs))
-		for i := 0; i < len(testCase.Txs); i++ {
-			vecTx := testCase.Txs[i]
-			if vecTx.Type != 0x0b {
-				tx, err := state.DecodeTx(vecTx.RawTx)
-				require.NoError(t, err)
-				l2Tx := state.L2TxRaw{
-					Tx:                   *tx,
-					EfficiencyPercentage: 255,
-				}
-				txs = append(txs, l2Tx)
+		// Run test cases
+		for i, testCase := range testCases {
+			block := state.Block{
+				BlockNumber: uint64(i + 1),
+				BlockHash:   state.ZeroHash,
+				ParentHash:  state.ZeroHash,
+				ReceivedAt:  time.Now(),
 			}
+
+			genesisActions := vectors.GenerateGenesisActionsEtrog(testCase.Genesis)
+
+			dbTx, err := testState.BeginStateTransaction(ctx)
+			require.NoError(t, err)
+
+			stateRoot, err := testState.SetGenesis(ctx, block, state.Genesis{Actions: genesisActions}, metrics.SynchronizerCallerLabel, dbTx)
+			require.NoError(t, err)
+			require.Equal(t, testCase.ExpectedOldStateRoot, stateRoot.String())
+			err = dbTx.Rollback(ctx)
+			require.NoError(t, err)
+
+			// convert vector txs
+			txs := make([]state.L2TxRaw, 0, len(testCase.Txs))
+			for i := 0; i < len(testCase.Txs); i++ {
+				vecTx := testCase.Txs[i]
+				if vecTx.Type != 0x0b {
+					tx, err := state.DecodeTx(vecTx.RawTx)
+					require.NoError(t, err)
+					l2Tx := state.L2TxRaw{
+						Tx:                   *tx,
+						EfficiencyPercentage: 255,
+					}
+					txs = append(txs, l2Tx)
+				}
+			}
+
+			timestampLimit, ok := big.NewInt(0).SetString(testCase.TimestampLimit, 10)
+			require.True(t, ok)
+
+			if len(txs) > 0 {
+				// Generate batchdata from the txs in the test and compared with the vector
+				l2block := state.L2BlockRaw{
+					DeltaTimestamp:  uint32(timestampLimit.Uint64()),
+					IndexL1InfoTree: testCase.Txs[0].IndexL1InfoTree,
+					Transactions:    txs,
+				}
+
+				batch := state.BatchRawV2{
+					Blocks: []state.L2BlockRaw{l2block},
+				}
+
+				batchData, err := state.EncodeBatchV2(&batch)
+				require.NoError(t, err)
+
+				require.Equal(t, common.FromHex(testCase.BatchL2Data), batchData)
+			}
+
+			processRequest := state.ProcessRequest{
+				BatchNumber: uint64(i + 1),
+				L1InfoTree_V2: state.L1InfoTreeExitRootStorageEntry{
+					L1InfoTreeRoot:  common.HexToHash(testCase.L1InfoRoot),
+					L1InfoTreeIndex: 0,
+				},
+				OldStateRoot:      stateRoot,
+				OldAccInputHash:   common.HexToHash(testCase.OldAccInputHash),
+				Transactions:      common.FromHex(testCase.BatchL2Data),
+				TimestampLimit_V2: timestampLimit.Uint64(),
+				Coinbase:          common.HexToAddress(testCase.SequencerAddress),
+				ForkID:            testCase.ForkID,
+			}
+
+			processResponse, _ := testState.ProcessBatchV2(ctx, processRequest, false)
+			require.Nil(t, processResponse.ExecutorError)
+			require.Equal(t, testCase.ExpectedNewStateRoot, processResponse.NewStateRoot.String())
 		}
-
-		timestampLimit, ok := big.NewInt(0).SetString(testCase.TimestampLimit, 10)
-		require.True(t, ok)
-
-		// Generate batchdata from the txs in the test and compared with the vector
-		l2block := state.L2BlockRaw{
-			DeltaTimestamp:  uint32(timestampLimit.Uint64()),
-			IndexL1InfoTree: testCase.Txs[0].IndexL1InfoTree,
-			Transactions:    txs,
-		}
-
-		batch := state.BatchRawV2{
-			Blocks: []state.L2BlockRaw{l2block},
-		}
-
-		batchData, err := state.EncodeBatchV2(&batch)
-		require.NoError(t, err)
-
-		require.Equal(t, common.FromHex(testCase.BatchL2Data), batchData)
-
-		processRequest := state.ProcessRequest{
-			BatchNumber: uint64(i + 1),
-			L1InfoTree_V2: state.L1InfoTreeExitRootStorageEntry{
-				L1InfoTreeRoot:  common.HexToHash(testCase.L1InfoRoot),
-				L1InfoTreeIndex: 0,
-			},
-			OldStateRoot:      stateRoot,
-			OldAccInputHash:   common.HexToHash(testCase.OldAccInputHash),
-			Transactions:      common.FromHex(testCase.BatchL2Data),
-			TimestampLimit_V2: timestampLimit.Uint64(),
-			Coinbase:          common.HexToAddress(testCase.SequencerAddress),
-			ForkID:            testCase.ForkID,
-		}
-
-		processResponse, _ := testState.ProcessBatchV2(ctx, processRequest, false)
-		require.Nil(t, processResponse.ExecutorError)
-		require.Equal(t, testCase.ExpectedNewStateRoot, processResponse.NewStateRoot.String())
 	}
 }
