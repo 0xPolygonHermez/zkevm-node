5ee8bdad4e86cb398d8b785e89188558ea035020
Toni Ramírez <58293609+ToniRamirezM@users.noreply.github.com>
2024-01-05 11:02:06 +0100

    Fix delta stream etrog (#3022)
    * fix delta stream for etrog
    
    * fix query for backwards compatibility
    
    * fix query for backwards compatibility
    
    * fix
    
    * add labels to loops
    
    * fix
    
    * fix
    
    * fix
    
    * add sanity check when adding l2blocks
    
    * fix
    
    * fix
    
    * fix
    
    * fix

--- sequencer/datastreamer.go
+++ sequencer/datastreamer.go
@@ -10,14 +10,14 @@ func (f *finalizer) DSSendL2Block(batchNumber uint64, blockResponse *state.Proce
 	// Send data to streamer
 	if f.streamServer != nil {
 		l2Block := state.DSL2Block{
-			BatchNumber:    batchNumber,
-			L2BlockNumber:  blockResponse.BlockNumber,
-			Timestamp:      int64(blockResponse.Timestamp),
-			GlobalExitRoot: blockResponse.BlockInfoRoot, //TODO: is it ok?
-			Coinbase:       f.sequencerAddress,
-			ForkID:         uint16(forkID),
-			BlockHash:      blockResponse.BlockHash,
-			StateRoot:      blockResponse.BlockHash, //TODO: in etrog the blockhash is the block root
+			BatchNumber:   batchNumber,
+			L2BlockNumber: blockResponse.BlockNumber,
+			Timestamp:     int64(blockResponse.Timestamp),
+			GERorInfoRoot: blockResponse.BlockInfoRoot,
+			Coinbase:      f.sequencerAddress,
+			ForkID:        uint16(forkID),
+			BlockHash:     blockResponse.BlockHash,
+			StateRoot:     blockResponse.BlockHash, //From etrog the blockhash is the block root
 		}
 
 		l2Transactions := []state.DSL2Transaction{}
