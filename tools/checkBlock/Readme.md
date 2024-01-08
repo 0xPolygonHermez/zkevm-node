9b99da84245086002042de4c05e9fcf5727c288c
Alonso <ARR551@protonmail.com>
2024-01-08 10:38:16 +0100

    block hash tool

---
+++ tools/checkBlock/Readme.md
@@ -0,0 +1,5 @@
+# Commands example:
+```
+go run main.go getBlockHash --url http://localhost:8123 --hash 0xded16e027c9d7b1244444f200a12de6932ddf6bd32e3cb4bb4f2ee138831ca69
+go run main.go readBlockHash --url http://localhost:8123 -n 93
+```
---
+++ tools/checkBlock/main.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"math/big"
+	"os"
+	"context"
+
+	"github.com/0xPolygonHermez/zkevm-node/log"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/urfave/cli/v2"
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "BlockTool"
+	app.Version = "v0.0.1"
+	flags := []cli.Flag{
+		&cli.StringFlag{
+			Name:    "url",
+			Aliases: []string{"u"},
+			Value:   "http://localhost:8123",
+		},
+	}
+	app.Commands = []*cli.Command{
+		{
+			Name:    "getBlockHash",
+			Aliases: []string{},
+			Usage:   "gets Block by Hash",
+			Action:  getBlockHash,
+			Flags:   append(flags, &cli.StringFlag{
+				Name:    "hash",
+				Aliases: []string{"bh"},
+				Value:   common.Hash{}.String(),
+			}),
+		},
+		{
+			Name:    "readBlockHash",
+			Aliases: []string{},
+			Usage:   "reads the block hash",
+			Action:  readBlockHash,
+			Flags:   append(flags, &cli.Uint64Flag{
+				Name:    "blocknumber",
+				Aliases: []string{"n"},
+				Value:   1,
+			}),
+		},
+	}
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Errorf("\nError: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func getBlockHash(ctx *cli.Context) error {
+	blockHash := common.HexToHash(ctx.String("hash"))
+	url := ctx.String("url")
+	getBlockByHash(ctx.Context, url, blockHash)
+	return nil
+}
+
+func readBlockHash(ctx *cli.Context) error {
+	blockNumber := ctx.Int64("blocknumber")
+	url := ctx.String("url")
+	block, err := getBlockByNumber(ctx.Context, url, blockNumber)
+	if err != nil {
+		log.Error("error in getBlockByNumber. Error: ", err)
+		return err
+	}
+	log.Infof("Block full header read (block %d): %+v", block.NumberU64(), block.Header())
+	log.Infof("Block stateRoot read (block %d): %+v", block.NumberU64(), block.Header().Root)
+	log.Infof("Block computed hash (block %d): %+v", block.NumberU64(), block.Header().Hash())
+	log.Infof("Block ReceiptHash read (block %d): %+v", block.NumberU64(), block.Header().ReceiptHash)
+	return nil
+}
+
+func getBlockByNumber(ctx context.Context, url string, blockNumber int64) (*types.Block, error) {
+	// Connect to ethereum node
+	ethClient, err := ethclient.Dial(url)
+	if err != nil {
+		log.Errorf("error connecting to %s: %+v", url, err)
+		return nil, err
+	}
+	bn := big.NewInt(int64(blockNumber))
+	block, err := ethClient.BlockByNumber(ctx, bn)
+	if err != nil {
+		log.Errorf("error getting block number: %d. Error: %v", blockNumber, err)
+		return nil, err
+	}
+	return block, nil
+}
+
+func getBlockByHash(ctx context.Context, url string, hash common.Hash) {
+	// Connect to ethereum node
+	ethClient, err := ethclient.Dial(url)
+	if err != nil {
+		log.Fatalf("error connecting to %s: %+v", url, err)
+	}
+
+	blockByHash, err := ethClient.BlockByHash(ctx, hash)
+	if err != nil {
+		log.Errorf("error getting block by computed hash: %s. Error: %v", hash, err)
+	} else {
+		log.Info("blockByHash:", hash, blockByHash.Hash())
+	}
+}
