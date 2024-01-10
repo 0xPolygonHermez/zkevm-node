1389c1b4fe323e0cf053457c064fd0dca4ecbd0e
trunghai95 <bthai.1995@gmail.com>
2024-01-10 13:39:31 +0800

    Fix lint

--- log/utils.go
+++ log/utils.go
@@ -10,10 +10,12 @@ const (
 	ctxKeyLogger contextKey = "logger"
 )
 
+// CtxWithLogger injects a logger into the context
 func CtxWithLogger(parentCtx context.Context, logger *Logger) context.Context {
 	return context.WithValue(parentCtx, ctxKeyLogger, logger)
 }
 
+// LoggerFromCtx returns the logger injected to context. If there is no logger, return the default logger
 func LoggerFromCtx(ctx context.Context) *Logger {
 	res := ctx.Value(ctxKeyLogger)
 	if res == nil {
