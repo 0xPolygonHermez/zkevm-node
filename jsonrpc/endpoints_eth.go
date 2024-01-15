fca35c4d94f1a4f6f9c1d3d0cb3d3b9c581a1749
Louis Liu <35095310+louisliu2048@users.noreply.github.com>
2024-01-15 14:12:49 +0800

    set eth_getLogs default value of fromBlock to "latest" (#70)
    * set eth_getLogs default value of fromBlock to "latest"
    
    * update unit test
    
    * update unit test
    
    * only update internalGetLogs func, not to modify GetNumericBlockNumbers function
    
    * only update GetLogs func, not to modify internalGetLogs func
    
    ---------
    
    Co-authored-by: Louis Liu <louis.liu@okg.com>

--- jsonrpc/endpoints_eth.go
+++ jsonrpc/endpoints_eth.go
@@ -492,6 +492,11 @@ func (e *EthEndpoints) GetFilterLogs(filterID string) (interface{}, types.Error)
 // GetLogs returns a list of logs accordingly to the provided filter
 func (e *EthEndpoints) GetLogs(filter LogFilter) (interface{}, types.Error) {
 	return e.txMan.NewDbTxScope(e.state, func(ctx context.Context, dbTx pgx.Tx) (interface{}, types.Error) {
+		if filter.FromBlock == nil {
+			bn := types.LatestBlockNumber
+			filter.FromBlock = &bn
+		}
+
 		return e.internalGetLogs(ctx, dbTx, filter)
 	})
 }
