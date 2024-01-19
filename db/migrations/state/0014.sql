2c4c5f1b02df6fb0b8bead709426e5867f971d1c
tclemos <thiago@polygon.technology>
2024-01-19 09:31:42 -0300

    fix index

--- db/migrations/state/0014.sql
+++ db/migrations/state/0014.sql
@@ -1,5 +1,5 @@
 -- +migrate Up
-CREATE INDEX IF NOT EXISTS idx_batch_global_exit_root ON state.batch (global_exit_root);
+CREATE INDEX IF NOT EXISTS idx_batch_global_exit_root ON state.batch (global_exit_root, batch_num);
 
 -- +migrate Down
 DROP INDEX IF EXISTS state.idx_batch_global_exit_root;
