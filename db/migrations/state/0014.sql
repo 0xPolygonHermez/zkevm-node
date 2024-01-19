937c57e9aefb80c141f913d1bde45c96018da158
tclemos <thiago@polygon.technology>
2024-01-19 09:36:23 -0300

    Revert "fix index"
    This reverts commit 2c4c5f1b02df6fb0b8bead709426e5867f971d1c.

--- db/migrations/state/0014.sql
+++ db/migrations/state/0014.sql
@@ -1,5 +1,5 @@
 -- +migrate Up
-CREATE INDEX IF NOT EXISTS idx_batch_global_exit_root ON state.batch (global_exit_root, batch_num);
+CREATE INDEX IF NOT EXISTS idx_batch_global_exit_root ON state.batch (global_exit_root);
 
 -- +migrate Down
 DROP INDEX IF EXISTS state.idx_batch_global_exit_root;
