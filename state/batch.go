6cca395e0d14c6ced3266fa09fdffe7eaead8c54
agnusmor <agnusmor@gmail.com>
2024-01-23 18:09:33 +0100

    fix migration batch wip field. fix sequence when no wip batch

--- state/batch.go
+++ state/batch.go
@@ -70,7 +70,7 @@ const (
 	// ForcedBatchDeadlineClosingReason is the closing reason used when forced batch deadline is reached
 	ForcedBatchDeadlineClosingReason ClosingReason = "Forced batch deadline"
 	// MaxDeltaTimestampClosingReason is the closing reason used when max delta batch timestamp is reached
-	MaxDeltaTimestampClosingReason ClosingReason = "Max delta timestamp delta"
+	MaxDeltaTimestampClosingReason ClosingReason = "Max delta timestamp"
 	// NoTxFitsClosingReason is the closing reason used when any of the txs in the pool (worker) fits in the remaining resources of the batch
 	NoTxFitsClosingReason ClosingReason = "No transactions fits"
 )
