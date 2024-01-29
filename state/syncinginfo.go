e847b3056aeaa241dd4a9695ae045da7cd68ef1a
joanestebanr <129153821+joanestebanr@users.noreply.github.com>
2024-01-29 17:39:53 +0100

    wip

--- state/syncinginfo.go
+++ state/syncinginfo.go
@@ -7,24 +7,21 @@ import (
 	"github.com/jackc/pgx/v4"
 )
 
-// SyncingInfo stores information regarding the syncing status of the node
-type SyncingInfo struct {
-	InitialSyncingBlock         uint64 // L2Block corresponding to InitialSyncingBatch
-	CurrentBlockNumber          uint64 // last L2Block in state
-	EstimatedHighestBlock       uint64 // estimated highest L2Block in state
-	InitialSyncingBatch         uint64
-	LastBatchNumberSeen         uint64
-	LastBatchNumberConsolidated uint64
-	CurrentBatchNumber          uint64
-	// IsSynchronizing indicates if the node is syncing (true -> syncing, false -> fully synced)
-	IsSynchronizing bool
-}
-
 // SyncInfoDataOnStorage stores information regarding the syncing status of the node in the database
 type SyncInfoDataOnStorage struct {
 	InitialSyncingBatch         uint64
 	LastBatchNumberSeen         uint64
 	LastBatchNumberConsolidated uint64
+}
+
+// SyncingInfo stores information regarding the syncing status of the node
+type SyncingInfo struct {
+	InitialSyncingBlock   uint64 // L2Block corresponding to InitialSyncingBatch
+	CurrentBlockNumber    uint64 // last L2Block in state
+	EstimatedHighestBlock uint64 // estimated highest L2Block in state
+
+	// IsSynchronizing indicates if the node is syncing (true -> syncing, false -> fully synced)
+	IsSynchronizing bool
 }
 
 // GetSyncingInfo returns information regarding the syncing status of the node
@@ -59,9 +56,9 @@ func (p *State) GetSyncingInfo(ctx context.Context, dbTx pgx.Tx) (SyncingInfo, e
 	} else if err != nil {
 		return SyncingInfo{}, err
 	}
-	info.CurrentBatchNumber = lastBatchNumber
+
 	info.EstimatedHighestBlock = ^uint64(0)
-	info.IsSynchronizing = info.LastBatchNumberSeen > lastBatchNumber
+	info.IsSynchronizing = syncData.LastBatchNumberSeen > lastBatchNumber
 
 	return info, nil
 }
