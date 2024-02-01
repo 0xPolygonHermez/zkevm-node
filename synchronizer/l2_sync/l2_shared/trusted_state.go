558362225a28c0608cac7651c9188a0bf1fb3346
Joan Esteban <129153821+joanestebanr@users.noreply.github.com>
2024-02-01 11:57:00 +0100

    synchronizer: update fromTrusted fix cache update (#3169)
    * fix cache update and sync from Trusted of closed batch

--- synchronizer/l2_sync/l2_shared/trusted_state.go
+++ synchronizer/l2_sync/l2_shared/trusted_state.go
@@ -31,6 +31,22 @@ func (ts *TrustedState) IsEmpty() bool {
 	return false
 }
 
+// GetCurrentBatch returns the current batch or nil
+func (ts *TrustedState) GetCurrentBatch() *state.Batch {
+	if ts == nil || len(ts.LastTrustedBatches) == 0 {
+		return nil
+	}
+	return ts.LastTrustedBatches[0]
+}
+
+// GetPreviousBatch returns the previous batch or nil
+func (ts *TrustedState) GetPreviousBatch() *state.Batch {
+	if ts == nil || len(ts.LastTrustedBatches) < 2 {
+		return nil
+	}
+	return ts.LastTrustedBatches[1]
+}
+
 // TrustedStateManager is the trusted state manager, basically contains the batch cache and create the TrustedState
 type TrustedStateManager struct {
 	Cache *common.Cache[uint64, *state.Batch]
