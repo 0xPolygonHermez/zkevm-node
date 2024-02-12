83dfb16d321daebe7963fc7d23aa3d109f7bb49e
joanestebanr <129153821+joanestebanr@users.noreply.github.com>
2024-02-12 13:03:50 +0100

    mainnet genesis for etrog

--- config/mainnetgenesis.go
+++ config/mainnetgenesis.go
@@ -5,8 +5,8 @@ const MainnetNetworkConfigJSON = `
 {
 	"l1Config" : {
 		"chainId": 1,
-		"polygonZkEVMAddress": "0x5132A183E9F3CB7C848b0AAC5Ae0c4f0491B7aB2",
-		"polTokenAddress": "0x7D1AfA7B718fb893dB30A3aBc0Cfc608AaCfeBB0",
+		"polygonZkEVMAddress": "0x519E42c24163192Dca44CD3fBDCEBF6be9130987",
+		"polTokenAddress": "0x455e53CBB86018Ac2B8092FdCd39d8444aFFC3F6",
 		"polygonZkEVMGlobalExitRootAddress": "0x580bda1e7A0CFAe92Fa7F6c20A3794F169CE3CFb"
 	},
     "root": "0x3f86b09b43e3e49a41fc20a07579b79eba044253367817d5c241d23c0e2bc5c9",
