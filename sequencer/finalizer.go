98289775bfbbfb0d75e20e36273a6af8b005f4bf
agnusmor <100322135+agnusmor@users.noreply.github.com>
2024-02-13 15:38:14 +0100

    fix node OOC (#3263)

--- sequencer/finalizer.go
+++ sequencer/finalizer.go
@@ -527,13 +527,43 @@ func (f *finalizer) handleProcessTransactionResponse(ctx context.Context, tx *Tx
 	}
 
 	// Check remaining resources
+
 	overflow, overflowResource := f.wipBatch.imRemainingResources.Sub(state.BatchResources{ZKCounters: result.UsedZkCounters, Bytes: uint64(len(tx.RawTx))})
 	if overflow {
 		log.Infof("current tx %s exceeds the remaining batch resources, overflow resource: %s, updating metadata for tx in worker and continuing", tx.HashStr, overflowResource)
-		start := time.Now()
-		f.workerIntf.UpdateTxZKCounters(result.BlockResponses[0].TransactionResponses[0].TxHash, tx.From, result.UsedZkCounters)
-		metrics.WorkerProcessingTime(time.Since(start))
-		return nil, ErrBatchResourceUnderFlow
+		if !f.batchConstraints.IsWithinConstraints(result.UsedZkCounters) {
+			log.Warnf("current tx %s exceeds the max limit for batch resources (node OOC), setting tx as invalid in the pool", tx.HashStr)
+
+			event := &event.Event{
+				ReceivedAt:  time.Now(),
+				Source:      event.Source_Node,
+				Component:   event.Component_Sequencer,
+				Level:       event.Level_Error,
+				EventID:     event.EventID_NodeOOC,
+				Description: fmt.Sprintf("tx: %s exceeds node max limit batch resources (node OOC), from: %s, IP: %s", tx.HashStr, tx.FromStr, tx.IP),
+			}
+
+			eventErr := f.eventLog.LogEvent(ctx, event)
+			if eventErr != nil {
+				log.Errorf("error storing finalizer halt event, error: %v", eventErr)
+			}
+
+			// Delete the transaction from the txSorted list
+			f.workerIntf.DeleteTx(tx.Hash, tx.From)
+
+			errMsg := "node OOC"
+			err = f.poolIntf.UpdateTxStatus(ctx, tx.Hash, pool.TxStatusInvalid, false, &errMsg)
+			if err != nil {
+				log.Errorf("failed to update status to invalid in the pool for tx %s, error: %v", tx.Hash.String(), err)
+			}
+
+			return nil, ErrBatchResourceUnderFlow
+		} else {
+			start := time.Now()
+			f.workerIntf.UpdateTxZKCounters(result.BlockResponses[0].TransactionResponses[0].TxHash, tx.From, result.UsedZkCounters)
+			metrics.WorkerProcessingTime(time.Since(start))
+			return nil, ErrBatchResourceUnderFlow
+		}
 	}
 
 	// Save Enabled, GasPriceOC, BalanceOC and final effective gas price for later logging
