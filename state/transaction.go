eb77e0406d454939728feeea1021d243edde70a8
tclemos <thiago@polygon.technology>
2024-02-17 11:24:24 -0300

    fix deltaTimeStamp for unsigned txs execution for specific block

--- state/transaction.go
+++ state/transaction.go
@@ -490,8 +490,23 @@ func (s *State) internalProcessUnsignedTransactionV2(ctx context.Context, tx *ty
 	}
 	nonce := loadedNonce.Uint64()
 
-	deltaTimestamp := uint32(uint64(time.Now().Unix()) - l2Block.Time())
-	transactions := s.BuildChangeL2Block(deltaTimestamp, uint32(0))
+	var oldStateRoot common.Hash
+	previousL2BlockNumber := uint64(0)
+	if l2Block.NumberU64() > 0 {
+		previousL2BlockNumber = l2Block.NumberU64() - 1
+	}
+	previousL2Block, err := s.GetL2BlockByNumber(ctx, previousL2BlockNumber, dbTx)
+	if err != nil {
+		return nil, err
+	}
+	oldStateRoot = previousL2Block.Root()
+
+	l1InfoTree, err := s.GetL1InfoRootLeafByL1InfoRoot(ctx, l2Block.BlockInfoRoot(), nil)
+	if err != nil {
+		return nil, err
+	}
+	deltaTimestamp := uint32(l2Block.Time() - previousL2Block.Time())
+	transactions := s.BuildChangeL2Block(deltaTimestamp, l1InfoTree.L1InfoTreeIndex)
 
 	batchL2Data, err := EncodeUnsignedTransaction(*tx, s.cfg.ChainID, &nonce, forkID)
 	if err != nil {
@@ -505,7 +520,7 @@ func (s *State) internalProcessUnsignedTransactionV2(ctx context.Context, tx *ty
 	processBatchRequestV2 := &executor.ProcessBatchRequestV2{
 		From:             senderAddress.String(),
 		OldBatchNum:      batch.BatchNumber,
-		OldStateRoot:     l2Block.Root().Bytes(),
+		OldStateRoot:     oldStateRoot.Bytes(),
 		OldAccInputHash:  batch.AccInputHash.Bytes(),
 		Coinbase:         batch.Coinbase.String(),
 		ForkId:           forkID,
