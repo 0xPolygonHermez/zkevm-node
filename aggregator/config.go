ce9f57c63163bdbc915f57519e40df39ffdb6fce
agnusmor <agnusmor@gmail.com>
2024-02-19 17:31:54 +0100

    add aggregator.BatchProofL1BlockConfirmations config parameter

--- aggregator/config.go
+++ aggregator/config.go
@@ -88,4 +88,7 @@ type Config struct {
 
 	// UpgradeEtrogBatchNumber is the number of the first batch after upgrading to etrog
 	UpgradeEtrogBatchNumber uint64 `mapstructure:"UpgradeEtrogBatchNumber"`
+
+	// BatchProofL1BlockConfirmations is number of L1 blocks to consider we can generate the proof for a virtual batch
+	BatchProofL1BlockConfirmations uint64 `mapstructure:"BatchProofL1BlockConfirmations"`
 }
