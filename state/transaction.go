e75f388b4a187a757c14991bdec56520474cdbc8
Toni Ramírez <toni@polygon.technology>
2024-02-20 15:41:31 +0100

    readd check

--- state/transaction.go
+++ state/transaction.go
@@ -1021,6 +1021,12 @@ func (s *State) internalTestGasEstimationTransactionV2(ctx context.Context, batc
 		s.eventLog.LogExecutorErrorV2(ctx, processBatchResponseV2.Error, processBatchRequestV2)
 		return false, false, gasUsed, nil, err
 	}
+	if processBatchResponseV2.ErrorRom != executor.RomError_ROM_ERROR_NO_ERROR {
+		err = executor.RomErr(processBatchResponseV2.ErrorRom)
+		s.eventLog.LogExecutorErrorV2(ctx, processBatchResponseV2.Error, processBatchRequestV2)
+		return false, false, gasUsed, nil, err
+	}
+
 	gasUsed = processBatchResponseV2.BlockResponses[0].GasUsed
 
 	txResponse := processBatchResponseV2.BlockResponses[0].Responses[0]
