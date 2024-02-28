4bd2089639d574c2dc9d59c95e3fed467f221e9b
agnusmor <100322135+agnusmor@users.noreply.github.com>
2024-02-28 17:05:36 +0100

    fix metrics estimatedTxsPerSec (#3396)

--- sequencer/metrics.go
+++ sequencer/metrics.go
@@ -81,7 +81,10 @@ func (m *metrics) close(createdAt time.Time, txsCount int64) {
 	if m.txsCount > 0 {
 		// timePerTxuS is the average time spent per tx. This includes the l2Block time since the processing time of this section is proportional to the number of txs
 		timePerTxuS := (m.transactionsTimes.total() + m.l2BlockTimes.total()).Microseconds() / m.txsCount
-		m.estimatedTxsPerSec = float64(m.totalTime().Microseconds()-m.newL2BlockTimes.total().Microseconds()) / float64(timePerTxuS)
+		// estimatedTxs is the number of transactions that we estimate could have been processed in the block
+		estimatedTxs := float64(totalTime.Microseconds()-m.newL2BlockTimes.total().Microseconds()) / float64(timePerTxuS)
+		// estimatedTxxPerSec is the estimated transactions per second
+		m.estimatedTxsPerSec = float64(totalTime.Microseconds()) / estimatedTxs
 	}
 }
 
@@ -151,7 +154,11 @@ func (i *intervalMetrics) addL2BlockMetrics(l2Block metrics) {
 }
 
 func (i *intervalMetrics) computeEstimatedTxsPerSec() {
-	i.estimatedTxsPerSec = i.estimatedTxsPerSecAcc / float64(i.estimatedTxsPerSecCount)
+	if i.estimatedTxsPerSecCount > 0 {
+		i.estimatedTxsPerSec = i.estimatedTxsPerSecAcc / float64(i.estimatedTxsPerSecCount)
+	} else {
+		i.estimatedTxsPerSecCount = 0
+	}
 }
 
 func (i *intervalMetrics) startsAt() time.Time {
