23a4ca45f1b0b75947ab5fc72f14832c9d6f0fe9
zhangkai <zhangkai.gis@163.com>
2024-03-05 17:58:28 +0800

    pr81 fix guage set

--- metrics/prometheus_x1.go
+++ metrics/prometheus_x1.go
@@ -63,7 +63,7 @@ func GaugeVecSet(name string, label string, value float64) {
 	}
 
 	if cv, ok := GaugeVec(name); ok {
-		cv.WithLabelValues(label).Add(value)
+		cv.WithLabelValues(label).Set(value)
 	}
 }
 
