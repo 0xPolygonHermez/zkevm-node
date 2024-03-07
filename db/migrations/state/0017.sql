57c515f494db1ec648df3ad79029644799085b56
Toni Ramírez <toni@polygon.technology>
2024-03-07 17:45:52 +0100

    fix migration

--- db/migrations/state/0017.sql
+++ db/migrations/state/0017.sql
@@ -6,7 +6,7 @@ CREATE INDEX IF NOT EXISTS l2block_block_num_idx ON state.l2block (block_num);
 UPDATE state.receipt SET im_state_root = post_state WHERE block_num >= (SELECT MIN(block_num) FROM state.l2block WHERE batch_num >= (SELECT from_batch_num FROM state.fork_id WHERE fork_id = 7));
 
 -- +migrate Down
-DROP INDEX IF EXISTS l2block_block_num_idx;
+DROP INDEX IF EXISTS state.l2block_block_num_idx;
 
 ALTER TABLE state.receipt
     DROP COLUMN IF EXISTS im_state_root;
